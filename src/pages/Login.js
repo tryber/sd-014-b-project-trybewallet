43742f10fe284fbc2364addd50f23d7c6e652215
Michael <michaelcaxias.dev@gmail.com>
2021-10-13 20:19:54 -0300

    Requisito 1: Completo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <input
+          type="text"
+          placeholder="Email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+        />
+        <button type="button">Entrar</button>
+      </section>
+    );
   }
 }
 
