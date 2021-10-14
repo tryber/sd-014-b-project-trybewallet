070d3b1e72df5ea1a839d91405772f3f699ea410
belaguarino <izabelaguarino@gmail.com>
2021-10-14 10:40:48 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,29 @@
+// Requisito 1: criação do campo de login
 import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="input-email">
+            <input
+              type="text"
+              placeholder="Email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="input-password">
+            <input
+              type="password"
+              placeholder="Senha"
+              data-testid="password-input"
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
