f8434cc2105ecb214ec81936cf23345a1ef5907d
eliHC <leonardohcds@gmail.com>
2021-10-14 21:11:32 -0300

    req 1 and 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,62 @@
 import React from 'react';
 
+const validateEmail = (email) => {
+  const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return regexEmail.test(email) === true;
+};
+
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    const minLength = 6;
+    const disabled = password.length >= minLength && validateEmail(email);
+
+    return (
+      <form>
+        <label htmlFor="email">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={ email }
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={ password }
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ !disabled }
+          // onClick={  }
+        >
+          Entrar
+        </button>
+      </form>);
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const walletReducer = (state = INITIAL_STATE) => state;
+
+export default walletReducer;
