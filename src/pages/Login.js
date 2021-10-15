971b80e732b68daec345091255d25863a600bbfe
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-10-15 15:36:59 -0300

    impl: store, rootReducer and refac inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,50 @@ import React from 'react';
 import styles from '../styles/login.module.scss';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { handleChange } = this;
+    const { email, password } = this.state;
+
     return (
       <form className={ styles.loginForm }>
-        <input data-testid="email-input" type="text" placeholder="Email" />
-        <input data-testid="password-input" type="text" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={ email }
+          onChange={ (event) => handleChange(event) }
+        />
+        <input
+          data-testid="password-input"
+          type="text"
+          name="password"
+          placeholder="Senha"
+          value={ password }
+          onChange={ (event) => handleChange(event) }
+        />
+        <button
+          type="submit"
+        >
+          Entrar
+        </button>
       </form>
     );
   }
