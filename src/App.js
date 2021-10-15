1648e5034af06ed6105089b18f8682c7a394a186
LucasAccurcio <lucas.accurcio@gmail.com>
2021-10-15 15:31:09 -0300

    Iniciando Projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  const history = createMemoryHistory();
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route history={ history } exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
