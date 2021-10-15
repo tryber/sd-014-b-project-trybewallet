70d895dba4c947ff2dbacacea34a47e43d497b13
Vinicius Pereira <pereiravinao@MacBook-Pro-de-Vinicius.local>
2021-10-15 18:24:42 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/carteira" component={ Wallet } />
         <Route path="/" component={ Login } />
       </Switch>
     );
