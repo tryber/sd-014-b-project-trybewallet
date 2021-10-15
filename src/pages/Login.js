3af8d1a18df2e968c0f88badd5d08a8e1a7d7683
lucas <mr.pine97@gmail.com>
2021-10-15 15:00:27 -0300

    configurando inputs e handleChange

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <form>
+        <label htmlFor="email-input">
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+        </label>
+      </form>
+    );
   }
 }
 
