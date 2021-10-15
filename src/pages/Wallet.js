35192b8b682bdccc9b56bcb192cf6f01f5fce2e3
enzo <enzo@pop-os.localdomain>
2021-10-15 19:11:44 -0300

    correção req 4

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import store from '../store/store';
 
 class Wallet extends React.Component {
   render() {
     return (
       <header>
-        <p data-testid="email-field"></p>
+        <p data-testid="email-field">{ store.getState().user.email}</p>
         <p data-testid="total-field"> 0</p>
         <p data-testid="header-currency-field">BRL</p>
       </header>
