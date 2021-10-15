3e33a313cdd1a58191e583696998495d19b5bc58
Guilherme <guimfontes7@gmail.com>
2021-10-15 18:19:22 -0300

    Renderiza página de login e faz testes nos campos de entrada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,65 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.enableButton = this.enableButton.bind(this);
+    this.state = {
+      password: '',
+      email: '',
+    };
+  }
+
+  componentDidUpdate() {
+    this.enableButton();
+  }
+
+  handleChange({ target }) {
+    const { type, value } = target;
+    this.setState({
+      [type]: value,
+    });
+  }
+
+  enableButton() {
+    const { password, email } = this.state;
+    /* eslint max-len: ["error", { "ignoreRegExpLiterals": true }] */
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const minLength = 6;
+    if (password.length >= minLength && regex.test(email)) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <h1>Login</h1>
+          <input
+            data-testid="email-input"
+            type="email"
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            onChange={ this.handleChange }
+          />
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ this.enableButton() }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,6 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
 const initialState = {
   user: {
     email: '',
