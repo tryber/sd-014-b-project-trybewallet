05ff2038cde076c67995c0a5472cff96d3b125f9
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-10-15 16:02:56 -0300

    Prepara merge

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from '../slices/loginSlice';
-
-export default configureStore({
-  reducer: {
-    login: loginReducer,
-  },
-});
