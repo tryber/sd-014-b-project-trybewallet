a2f067298d6fe12c35c565d200bfff0239a87e89
Emídio Rebouças <emidioreb@gmail.com>
2021-10-15 15:44:31 -0300

    requisito 1 ok

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,29 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class Input extends React.Component {
   render() {
-    return ()
+    const { type, name, placeholder, id } = this.props;
+    return (
+      <div>
+        <input
+          type={ type }
+          name={ name }
+          data-testid={ id }
+          // value={ value }
+          // onChange={ onChange }
+          placeholder={ placeholder }
+        />
+      </div>
+    );
   }
 }
+
+Input.propTypes = {
+  id: PropTypes.isRequired,
+  name: PropTypes.isRequired,
+  placeholder: PropTypes.isRequired,
+  type: PropTypes.isRequired,
+};
 
 export default Input;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Button from '../components/Button';
+import Input from '../components/Input';
 
 class Login extends React.Component {
   render() {
     return (
-      <Button id="button-login" label="Entrar" />
+      <div>
+        <Input
+          id="email-input"
+          type="text"
+          name="email"
+          placeholder="exemple@email.com"
+        />
+        <Input
+          id="password-input"
+          type="password"
+          name="senha"
+          placeholder="Digite sua senha"
+        />
+        <Button id="button-login" label="Entrar" />
+      </div>
     );
   }
 }
