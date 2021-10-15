dc133adb5391bcda1b8b5f27fca1ed0537bfd78f
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-10-15 16:33:37 -0300

    Atende requisito #2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,64 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disabled: true,
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  isEmailValid(email) {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(email);
+  }
+
+  validate() {
+    const { email, password } = this.state;
+    const magicNumber = 5;
+    if ((password.length >= magicNumber) && (this.isEmailValid(email))) {
+      this.setState({
+        disabled: false,
+      });
+    }
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.id]: target.value,
+    });
+    this.validate();
+  }
+
   render() {
+    const { disabled, email, password } = this.state;
     return (
       <div>
         Login
         <label htmlFor="email">
           Email:
-          <input type="text" id="email" data-testid="email-input" />
+          <input
+            type="email"
+            id="email"
+            value={ email }
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
         </label>
-        <label htmlFor="senha">
+        <label htmlFor="password">
           Senha:
-          <input type="text" id="senha" data-testid="password-input" />
+          <input
+            type="text"
+            id="password"
+            value={ password }
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
         </label>
-        <button type="button">Entrar</button>
+        <button type="button" disabled={ disabled }>Entrar</button>
       </div>);
   }
 }
