71224ae7448cc54a51a12f97f452fe5b79798347
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-10-15 13:18:51 -0300

    Req 04

--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import Wallet from '../pages/Wallet';
 
 class Switcher extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
         <Route exact path="/" render={ (props) => <Login { ...props } /> } />
       </Switch>
     );
