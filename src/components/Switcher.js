27010344f1d38efab0130a94ae705bd99137620e
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-10-15 06:38:57 -0300

    Criando o componente Switcher.js

---
+++ src/components/Switcher.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Switch } from 'react-router-dom';
+
+class Switcher extends Component {
+  render() {
+    return (
+      <Switch />
+    );
+  }
+}
+
+export default Switcher;
