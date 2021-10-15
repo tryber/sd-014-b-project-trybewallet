a93e0e19b72983da675c69154c2dae413b72c308
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-10-15 15:09:15 -0300

    Cria uma página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+export default function Login() {
+  return (
+    <form>
+      <fieldset>
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+          placeholder="email@email.com"
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          placeholder="senha"
+          minLength="6"
+        />
+        <button type="button">Entrar</button>
+      </fieldset>
+    </form>
+  );
 }
-
-export default Login;
