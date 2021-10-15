98e37dfb9cfe446f95fa6241e5739ea0dd90c35d
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-10-15 16:03:24 -0300

    Configuraçao inicial do redux

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,10 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const initialState = {};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
