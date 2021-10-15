6b264799f3320cb0cc1fb0212088e6cf16fe72e5
Olávio Timóteo <olaviotimoteo@hotmail.com>
2021-10-15 15:42:13 -0300

    Adding requirement 1 and starting requirement 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
 // Coloque aqui suas actions
+const SAVE_EMAIL = 'SAVE_EMAIL';
+
+// Action para salvar na store o email.
+const saveEmailAction = (email) => ({
+  type: SAVE_EMAIL,
+  payload: email,
+});
+
+export default saveEmailAction;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) { // Função generica para pegar as alterações do input e salvar no estado do component.
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { email, password } = this.state;
+    console.log(this.state);
     return (
-      <h1>Login</h1>
+      <form>
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          type="text"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button type="button">Entrar</button>
+      </form>
     );
   }
 }
