91bc7f87dcce3f55c29bd23ee9f0d2f5f66cf52f
Israel <israel.fab@hotmail.com>
2021-10-15 15:38:03 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <input
+          type="text"
+          data-testid="email-input"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="Senha"
+        />
+        <button type="button">Entrar</button>
+      </section>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const userReducer = (state = INITIAL_STATE) => state;
+
+export default userReducer;
