f02e368368f1988ddcb6df420ffe40c6f759b09e
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-10-15 17:14:10 -0300

    Requisito 1 e 2 ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <Switch>
+        {/* <Route path="/wallet" component={ } /> */}
+        <Route path="/" component={ Login } />
+      </Switch>
+      <div>Hello, TrybeWallet!</div>
+
+    </>);
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,53 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  isEmailValid(email) {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(email);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const SIX = 6;
+    const disabled = password.length >= SIX && this.isEmailValid(email);
+    return (
+      <div>
+        Login
+        <input
+          data-testid="email-input"
+          name="email"
+          value={ email }
+          type="text"
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          type="password"
+          onChange={ this.handleChange }
+        />
+        <button type="button" disabled={ !disabled }>Entrar</button>
+
+      </div>
+
+    );
   }
 }
 
