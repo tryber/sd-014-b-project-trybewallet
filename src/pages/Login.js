f98e36c0ff63b7936ec93bad0986ac085fe7de09
Kelner Roberto <kelner250@hotmail.com>
2021-10-15 10:32:14 -0300

    requisito 1 - implementaçao da store redux, do devtools e do rootReducer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email } = this.state;
+    return (
+      <form>
+        <label htmlFor="email-login-input">
+          Email
+          <input
+            type="email"
+            id="email-login-input"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        Password
+        <label htmlFor="password-login-input">
+          <input
+            type="password"
+            id="password-login-input"
+            data-testid="password-input"
+          />
+        </label>
+        <button id="btn-login" type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
