8295c42afff73d4f4f54be80630dd6e812a955de
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-10-15 16:58:33 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <div>
+        <input
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
