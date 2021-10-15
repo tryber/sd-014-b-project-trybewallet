56349cb7f40e7b80ce5d2c06775956c5b955bf75
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-10-15 18:57:30 -0300

    refac: requisito 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,115 +1,115 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { addEmailAddress } from '../actions';
 import styles from '../styles/login.module.scss';
 
-// class Login extends React.Component {
-//   constructor() {
-//     super();
+class Login extends React.Component {
+  constructor() {
+    super();
 
-//     this.state = {
-//       email: '',
-//       password: '',
-//     };
+    this.state = {
+      email: '',
+      password: '',
+    };
 
-//     this.handleChange = this.handleChange.bind(this);
-//     this.validateEmailAndLogin = this.validateEmailAndLogin.bind(this);
-//   }
+    this.handleChange = this.handleChange.bind(this);
+    this.validateEmailAndLogin = this.validateEmailAndLogin.bind(this);
+  }
 
-//   handleChange({ target: { value, name } }) {
-//     this.setState({
-//       [name]: value,
-//     });
-//   }
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
 
-//   validateEmailAndLogin() {
-//     const { email, password } = this.state;
-//     const emailRegex = /\S+@\S+\.\S+/;
-//     const MIN_PASSWORD_LENGTH = 6;
-
-//     return emailRegex.test(email) && password.length >= MIN_PASSWORD_LENGTH;
-//   }
-
-//   render() {
-//     const { handleChange, validateEmailAndLogin } = this;
-//     const { email, password } = this.state;
-//     const { sendEmail } = this.props;
-
-//     return (
-//       <form className={ styles.loginForm }>
-//         <input
-//           data-testid="email-input"
-//           type="text"
-//           name="email"
-//           placeholder="Email"
-//           value={ email }
-//           onChange={ (event) => handleChange(event) }
-//         />
-//         <input
-//           data-testid="password-input"
-//           type="text"
-//           name="password"
-//           placeholder="Senha"
-//           value={ password }
-//           onChange={ (event) => handleChange(event) }
-//         />
-//         <Link to="/carteira">
-//           <button
-//             type="button"
-//             disabled={ !validateEmailAndLogin() }
-//             onClick={ () => sendEmail(email) }
-//           >
-//             Entrar
-//           </button>
-//         </Link>
-//       </form>
-//     );
-//   }
-// }
-
-function Login({ sendEmail }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const validateEmailAndLogin = () => {
+  validateEmailAndLogin() {
+    const { email, password } = this.state;
     const emailRegex = /\S+@\S+\.\S+/;
     const MIN_PASSWORD_LENGTH = 6;
 
     return emailRegex.test(email) && password.length >= MIN_PASSWORD_LENGTH;
-  };
+  }
 
-  return (
-    <form className={ styles.loginForm }>
-      <input
-        data-testid="email-input"
-        type="text"
-        name="email"
-        placeholder="Email"
-        value={ email }
-        onChange={ ({ target: { value } }) => setEmail(value) }
-      />
-      <input
-        data-testid="password-input"
-        type="text"
-        name="password"
-        placeholder="Senha"
-        value={ password }
-        onChange={ ({ target: { value } }) => setPassword(value) }
-      />
-      <Link to="/carteira">
-        <button
-          type="button"
-          disabled={ !validateEmailAndLogin() }
-          onClick={ () => sendEmail(email) }
-        >
-          Entrar
-        </button>
-      </Link>
-    </form>
-  );
+  render() {
+    const { handleChange, validateEmailAndLogin } = this;
+    const { email, password } = this.state;
+    const { sendEmail } = this.props;
+
+    return (
+      <form className={ styles.loginForm }>
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={ email }
+          onChange={ (event) => handleChange(event) }
+        />
+        <input
+          data-testid="password-input"
+          type="text"
+          name="password"
+          placeholder="Senha"
+          value={ password }
+          onChange={ (event) => handleChange(event) }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ !validateEmailAndLogin() }
+            onClick={ () => sendEmail(email) }
+          >
+            Entrar
+          </button>
+        </Link>
+      </form>
+    );
+  }
 }
+
+// function Login({ sendEmail }) {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const validateEmailAndLogin = () => {
+//     const emailRegex = /\S+@\S+\.\S+/;
+//     const MIN_PASSWORD_LENGTH = 6;
+
+//     return emailRegex.test(email) && password.length >= MIN_PASSWORD_LENGTH;
+//   };
+
+//   return (
+//     <form className={ styles.loginForm }>
+//       <input
+//         data-testid="email-input"
+//         type="text"
+//         name="email"
+//         placeholder="Email"
+//         value={ email }
+//         onChange={ ({ target: { value } }) => setEmail(value) }
+//       />
+//       <input
+//         data-testid="password-input"
+//         type="text"
+//         name="password"
+//         placeholder="Senha"
+//         value={ password }
+//         onChange={ ({ target: { value } }) => setPassword(value) }
+//       />
+//       <Link to="/carteira">
+//         <button
+//           type="button"
+//           disabled={ !validateEmailAndLogin() }
+//           onClick={ () => sendEmail(email) }
+//         >
+//           Entrar
+//         </button>
+//       </Link>
+//     </form>
+//   );
+// }
 
 const mapDispatchToProps = (dispatch) => ({
   sendEmail: (emailAddress) => dispatch(addEmailAddress(emailAddress)),
