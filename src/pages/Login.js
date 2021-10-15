f13014999699a916ad47d54ac23c2ba3298f16b4
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-15 18:23:03 -0300

    colocando {} na importação da actin por causa que não tem default na exportação

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import emailAction from '../actions';
+import { emailAction } from '../actions';
 
 class Login extends React.Component {
   constructor() {
