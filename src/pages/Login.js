438c6f45b0cad017043d50a2c2390b9d92d589b8
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-10-15 17:15:04 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,71 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChangeInput = this.handleChangeInput.bind(this);
+    this.validation = this.validation.bind(this);
+  }
+
+  handleChangeInput({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  // referências: https://www.w3resource.com/javascript/form/email-validation.php,
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+  validation(email) {
+    // Não consigo entender bem como funciona, mas a expressão abaixo é utilizada para validar
+    // se existem os caracters necessários para validar o email.
+    const emailValidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    // O metodo test() verifica se o email recebido por parametro está de acordo com as regras
+    // que foram passadas pela const emailValidation e retorna true ou false
+    return emailValidation.test(email);
+  }
+
   render() {
+    const { email, password } = this.state;
+    const MIN_CARACTER = 6;
+    const validation = (this.validation(email) && password.length >= MIN_CARACTER);
     return (
-      <section className="container mt-4">
-        <form className="row col-sm-6">
-          <label className="form-label" htmlFor="email">
-            <input
-              className="form-control"
-              id="email"
-              name="email"
-              data-testid="email-input"
-              placeholder="E-mail"
-            />
-          </label>
-          <label className="form-label" htmlFor="password">
-            <input
-              className="form-control"
-              id="password"
-              name="password"
-              data-testid="password-input"
-              placeholder="Senha"
-            />
-          </label>
-          <div className="container">
-            <button type="button" className="btn btn-success w-50">
+      <section className="container m-5">
+        <h1 className="display-6">Login</h1>
+        <form className="col-md-5">
+          <div>
+            <label className="form-label w-100" htmlFor="email">
+              <input
+                className="form-control"
+                id="email"
+                name="email"
+                data-testid="email-input"
+                placeholder="E-mail"
+                onChange={ this.handleChangeInput }
+              />
+            </label>
+          </div>
+          <div>
+            <label className="form-label w-100" htmlFor="password">
+              <input
+                className="form-control"
+                id="password"
+                name="password"
+                type="password"
+                data-testid="password-input"
+                placeholder="Senha"
+                onChange={ this.handleChangeInput }
+              />
+            </label>
+          </div>
+          <div>
+            <button
+              type="button"
+              className="btn btn-success w-100"
+              disabled={ !validation }
+            >
               Entrar
             </button>
           </div>
