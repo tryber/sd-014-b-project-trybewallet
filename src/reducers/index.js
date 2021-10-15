523934d351c29493af52ab17ac41c3e2d0d01a1e
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-15 14:46:41 -0300

    ajustando o provider no index já importando a store que futturamente vai ser criada

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,12 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const INISTIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
