6a5947adccf8984cd945ce7554036ee1a72d0036
Fernando Serpa <serpaef@outlook.com>
2021-10-15 18:08:05 -0300

    correcao do linter

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { doLogin } from '../actions';
 
 class Login extends React.Component {
@@ -78,5 +79,9 @@ class Login extends React.Component {
 const mapDispatchToProps = (dispatch) => ({
   dispatchEmail: (value) => dispatch(doLogin(value)),
 });
+
+Login.propTypes = {
+  dispatchEmail: PropTypes.func.isRequired,
+};
 
 export default connect(null, mapDispatchToProps)(Login);
