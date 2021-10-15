bde62e7a42270b52f28d8958a43bff2d0fbf0e2c
vanim <vanimouzad123@gmail.com>
2021-10-15 19:19:15 -0300

    Requisito 5

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  render() {
+    const { userEmail } = this.props;
+
+    return (
+      <>
+        <div data-testid="email-field">
+          Email:
+          { userEmail }
+        </div>
+        <div data-testid="total-field">
+          0
+        </div>
+        <span data-testid="header-currency-field">BRL</span>
+        <form>
+          <label htmlFor="input-name">
+            Nome:
+            <input id="input-name" type="text" />
+          </label>
+        </form>
+      </>
+    );
+  }
+}
+
+Header.propTypes = {
+  userEmail: PropTypes.shape({
+    user: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
+
+const mapStateToProps = (globalState) => ({
+  userEmail: globalState.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
