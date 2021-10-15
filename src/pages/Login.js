9f66d73c26458e6760f041f383471d6d964bcde8
Kelner Roberto <kelner250@hotmail.com>
2021-10-15 11:27:21 -0300

    requisito 2 - validaçao de email e senha pro login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,20 +6,40 @@ class Login extends React.Component {
 
     this.state = {
       email: '',
+      password: '',
+      disabled: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleDisabled = this.handleDisabled.bind(this);
   }
 
   handleChange({ target }) {
     const { value, name } = target;
     this.setState({
       [name]: value,
-    });
+    }, () => this.handleDisabled());
+  }
+
+  handleDisabled() {
+    const { email, password } = this.state;
+    const PASSWORD_MINIMUM_CHARACTERS = 6;
+    const checkEmail = email.includes('@');
+    const emailAdd = email.includes('.com');
+
+    if (password.length >= PASSWORD_MINIMUM_CHARACTERS && checkEmail && emailAdd) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
   }
 
   render() {
-    const { email } = this.state;
+    const { email, disabled } = this.state;
     return (
       <form>
         <label htmlFor="email-login-input">
@@ -39,9 +59,18 @@ class Login extends React.Component {
             type="password"
             id="password-login-input"
             data-testid="password-input"
+            onChange={ this.handleChange }
+            name="password"
           />
         </label>
-        <button id="btn-login" type="button">Entrar</button>
+        <button
+          id="btn-login"
+          type="button"
+          disabled={ disabled }
+        >
+          Entrar
+
+        </button>
       </form>
     );
   }
