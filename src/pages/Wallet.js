c788f65e26d7b86183ea6bc668b923879a9ae96e
ronaldosenajr <ronaldosenajr@gmail.com>
2021-10-15 18:03:14 -0400

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import InputBase from '../components/InputBase';
 
 class Wallet extends React.Component {
   render() {
@@ -7,6 +8,34 @@ class Wallet extends React.Component {
       <main>
         <Header />
         TrybeWallet
+        <form>
+          <InputBase name="Valor" type="number" />
+          <InputBase name="Descrição" type="text" />
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              <option>BRL</option>
+            </select>
+          </label>
+          <label htmlFor="metodo-de-pagamento">
+            Método de pagamento
+            <select id="metodo-de-pagamento">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="credito">Cartão de crédito</option>
+              <option value="debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transportes">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
       </main>);
   }
 }
