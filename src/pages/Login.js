42f42356b27cbd2124a61fbb796b6b3dc3bd8cd5
Gustavo <gus.inacio@hotmail.com>
2021-10-15 16:31:49 -0300

    impl: requirement #1 and material-ui lib.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import '../App.css';
+import { Container } from '@material-ui/core';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <Container maxWidth="sm">
+        <div>
+          <h2>Login</h2>
+          <label htmlFor="input-email">
+            Email:
+            <input
+              data-testid="email-input"
+              id="input-email"
+              name="email"
+              type="text"
+            />
+          </label>
+          <label htmlFor="input-password">
+            senha:
+            <input
+              data-testid="password-input"
+              type="password"
+              name="password"
+              id="input-password"
+            />
+          </label>
+          <button type="button">Entrar</button>
+        </div>
+      </Container>
+    );
   }
 }
 
