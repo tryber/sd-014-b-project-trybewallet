fa7325cb5d51ecd51ea3528581caecf679d30e95
vanim <vanimouzad123@gmail.com>
2021-10-15 16:10:12 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.isEmailValid = this.isEmailValid.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+
+    this.state = {
+      user: {
+        email: '',
+      },
+      passwordLength: 0,
+    };
+  }
+
+  // Função de validação de email
+  isEmailValid(email) {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(email); // retorna TRUE se regexEmail for válido
+  }
+
+  // Função para setar o email e o tamanho da senha do usuário do estado local
+  handleLogin(event) {
+    if (event.target.type === 'email') {
+      this.setState({
+        user: {
+          email: event.target.value,
+        },
+      });
+    }
+
+    if (event.target.type === 'password') {
+      this.setState({
+        passwordLength: event.target.value.length,
+      });
+    }
+  }
+
   render() {
+    const { user: { email }, passwordLength } = this.state;
+    const MIN_PASSWORD_LENGTH = 6;
+
     return (
-      <>
-        <input type="email" data-testid="email-input" />
-        <input type="password" data-testid="password-input" />
-        <button type="button">Entrar</button>
-      </>);
+      <div>
+        <input type="email" data-testid="email-input" onChange={ this.handleLogin } />
+        <input
+          type="password"
+          data-testid="password-input"
+          onChange={ this.handleLogin }
+        />
+        <Link to="/carteira">
+          <button
+            type="button"
+            // Se as duas condições retornarem false, disabled === false;
+            disabled={ !this.isEmailValid(email) || passwordLength < MIN_PASSWORD_LENGTH }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
