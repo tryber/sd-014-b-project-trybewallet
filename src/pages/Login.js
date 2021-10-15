350b6525361f8639427f6ad253bf8297e3580bf4
Elithon Silva <elithonsilva@gmail.com>
2021-10-15 14:57:11 -0300

    página inicial com login, password e button Entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <input
+          type="email"
+          placeholder="Digite seu e-mail"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          data-testid="password-input"
+        />
+        <button type="button">Entrar</button>
+      </section>
+    );
   }
 }
 
