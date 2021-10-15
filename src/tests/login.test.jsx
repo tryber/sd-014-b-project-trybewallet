18d02c8777d660b540e617f4c2453d3edcf04a2b
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-10-15 15:52:01 -0300

    Testa se a refatoração das pastas atrapalhou a avaliação

--- src/tests/login.test.jsx
+++ src/tests/login.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { fireEvent, screen } from '@testing-library/react';
-import App from '../App';
+import App from '../app/App';
 
 import { renderWithRouterAndStore } from './testConfig';
 
