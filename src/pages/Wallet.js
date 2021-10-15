26408a2d32fc98ad3d8bc9d0a9a1fc589071585b
Thyara <thyaranonato@outlook.com>
2021-10-15 18:01:08 -0300

    Impl: requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,14 +1,66 @@
 import React from 'react';
-import Header from './Header';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
+    const { email } = this.props;
     return (
       <section>
-        <Header />
+        <header>
+          <p data-testid="email-field">
+            Email:
+            {' '}
+            { email }
+          </p>
+          <p data-testid="total-field">Despesa total: 0</p>
+          <p data-testid="header-currency-field">Câmbio utilizado: BRL</p>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input type="text" name="value" id="value" />
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input type="text" name="description" id="description" />
+          </label>
+          <label htmlFor="coin">
+            Moeda
+            <select type="text" name="coin" id="coin">
+              <option value="coin">Selecione a Moeda</option>
+            </select>
+          </label>
+          <label htmlFor="payment-method">
+            Método de Pagamento
+            <select type="text" name="payment-method" id="payment-method">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="credito">Cartão de crédito</option>
+              <option value="debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select type="text" name="tag" id="tag">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
       </section>
     );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
