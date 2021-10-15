c8ebc2998c83474489417e1af9dba4906355effa
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-10-15 17:46:49 -0300

    Config Redux, initial store and Reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_USER_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_USER_STATE, action) => {
+  switch (action.type) {
+  case 'action_type':
+    return {
+      ...state,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
