e4907aa3acf3167b26488fa7fe2805225381455f
Lombeira <glomenha@gmail.com>
2021-10-15 17:43:35 -0300

    No Silveira, no trybe

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const SEND_EMAIL_ADDRESS = 'SEND_EMAIL_ADDRESS';
+
+export const addEmailAddress = (emailAddress) => ({
+  type: SEND_EMAIL_ADDRESS,
+  emailAddress,
+});
