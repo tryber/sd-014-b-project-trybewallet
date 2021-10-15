a772319ee07c9a6557de0b1386a303ee77f956ef
João Reis <joaoreisjpk@gmail.com>
2021-10-15 14:42:08 -0300

    lint issues

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,26 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense as removeExpenseAction } from '../actions';
 
 class Table extends React.Component {
+  header = () => (
+    <>
+      <th>Descrição</th>
+      <th>Tag</th>
+      <th>Método de pagamento</th>
+      <th>Valor</th>
+      <th>Moeda</th>
+      <th>Câmbio utilizado</th>
+      <th>Valor convertido</th>
+      <th>Moeda de conversão</th>
+      <th>Editar/Excluir</th>
+    </>
+  )
+
   render() {
     const { gastos, removeExpense, editing } = this.props;
-    return(
+    return (
       <table>
         <thead>
           <tr>
@@ -20,45 +35,55 @@ class Table extends React.Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
-        <tbody>{ gastos && gastos.length && gastos.map((item) => {
-              const currency = item.currency;
-              const currencyName = item.exchangeRates[currency].name.split('/')[0];
-              const currencyValue = item.exchangeRates[currency].ask;
-              const convert = currencyValue * item.value;
-              return (
-              <tr key={item.id} id={item.id}>
-                <td>{ item.description }</td>
-                <td>{ item.tag }</td>
-                <td>{ item.method }</td>
-                <td>{ item.value }</td>
+        { gastos && gastos.map((item) => {
+          const { currency, id, description, tag, method, value, exchangeRates } = item;
+          const currencyName = exchangeRates[currency].name.split('/')[0];
+          const currencyValue = exchangeRates[currency].ask;
+          const convert = currencyValue * value;
+          return (
+            <tbody key={ id }>
+              <tr id={ id }>
+                <td>{ description }</td>
+                <td>{ tag }</td>
+                <td>{ method }</td>
+                <td>{ value }</td>
                 <td>{ currencyName }</td>
                 <td>{ Number(currencyValue).toFixed(2) }</td>
                 <td>{ convert.toFixed(2) }</td>
-                <td>{ 'Real' }</td>
-                <td>{ item.currency }</td>
+                <td>Real</td>
+                <td>{ currency }</td>
                 <td>
                   <button
                     data-testid="delete-btn"
                     type="button"
-                    onClick={ () => removeExpense({ id:item.id })}
+                    onClick={ () => removeExpense({ id }) }
                   >
                     X
                   </button>
                   <button
                     data-testid="edit-btn"
                     type="button"
-                    onClick={ () => editing(item)}
+                    onClick={ () => editing(item) }
                   >
                     E
                   </button>
                 </td>
               </tr>
-              )
-            })}</tbody>
+            </tbody>
+          );
+        })}
       </table>
-    )
+    );
   }
 }
+
+Table.propTypes = {
+  editing: PropTypes.func.isRequired,
+  gastos: PropTypes.shape({
+    map: PropTypes.func,
+  }).isRequired,
+  removeExpense: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state) => ({
   gastos: state.wallet.expenses,
