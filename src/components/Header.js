9ed825eeb94cbd1408819cc9dc7afe3a37ae0548
Elithon Silva <elithonsilva@gmail.com>
2021-10-15 18:34:48 -0300

    Um elemento que exiba o email do usuário que fez login

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{ email }</span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
