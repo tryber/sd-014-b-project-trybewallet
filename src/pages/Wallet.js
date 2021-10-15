2661d0fab62a83613a426df6941b557628dac118
Polyana Balotin <polybalotin@hotmail.com>
2021-10-15 15:48:30 -0300

    requisitos 4 e 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      totalExpenses: 0,
+      currency: 'BRL',
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    const { totalExpenses, currency } = this.state;
+    return (
+      <header>
+        <span data-testid="email-field">{ email }</span>
+        <span data-testid="total-field">{ totalExpenses }</span>
+        <span data-testid="header-currency-field">{ currency }</span>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
