4763ac0de9bea466b036400cfbb3b1fe6617c0e1
lucas <mr.pine97@gmail.com>
2021-10-15 15:16:50 -0300

    começando a configuração do redux

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const initialState = {
+  user: {
+    email: '',
+  },
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+  case typeName:
+    return { ...state, ...payload };
+  default:
+    return state;
+  }
+};
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+const initialState = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+  case typeName:
+    return { ...state, ...payload };
+
+  default:
+    return state;
+  }
+};
