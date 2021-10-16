e50466b0000cfcf446d0e0691065aca3d231e7ae
daniel <daniel.acoelho@outlook.com>
2021-10-16 18:11:50 -0300

    teste req 2 parte 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class Wallet extends React.Component {
-  render() {
-    return <div>TrybeWallete</div>;
-  }
+export default function Wallet() {
+  return <div>TrybeWallet</div>;
 }
 
 export default Wallet;
