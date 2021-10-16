c43d0027f8baf130800b6a05d5373c4edd42c1e9
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-10-16 12:18:19 -0300

    1.feito ainda n passa pq n foi config. o redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,54 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // email: '',
+      // password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <fieldset>
+          <label
+            htmlFor="email"
+          >
+            Email
+            <input
+              id="email"
+              type="text"
+              name="email"
+              onChange={ this.handleChange }
+              data-testid="email-input"
+            />
+          </label>
+
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button type="button">Entrar</button>
+        </fieldset>
+      </form>
+    );
   }
 }
 
