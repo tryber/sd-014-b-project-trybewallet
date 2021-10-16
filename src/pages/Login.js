f61ffda3e80bae8f526b334f11e49c403c1e7a28
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-10-15 23:45:42 -0300

    [Rafael Moraes] Projeto TrybeWallet - Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      btnStatus: true,
+      email: '',
+      password: '',
+      statusPassword: true,
+      statusEmail: true,
+    };
+    this.updateBtnStatus = this.updateBtnStatus.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+  }
+
+  // Função abaixo retirada de https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+  validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  updateBtnStatus() {
+    const { statusPassword, statusEmail } = this.state;
+    if (!statusPassword && !statusEmail) {
+      this.setState({ btnStatus: false });
+    } else {
+      this.setState({ btnStatus: true });
+    }
+  }
+
   render() {
+    const { btnStatus, email, password } = this.state;
+    const minNumber = 6;
+    console.log(email, password);
     return (
       <main>
         <label htmlFor="email-input">
@@ -9,6 +41,20 @@ class Login extends React.Component {
             type="text"
             placeholder="Digite seu e-mail"
             data-testid="email-input"
+            onChange={ ({ target }) => {
+              const statusEmail = this.validateEmail(target.value);
+              if (statusEmail) {
+                this.setState({
+                  email: target.value,
+                  statusEmail: false,
+                }, () => this.updateBtnStatus());
+              } else {
+                this.setState({
+                  email: target.value,
+                  statusEmail: true,
+                }, () => this.updateBtnStatus());
+              }
+            } }
           />
         </label>
         <label htmlFor="password-input">
@@ -16,9 +62,20 @@ class Login extends React.Component {
             type="text"
             placeholder="Digite sua senha"
             data-testid="password-input"
+            onChange={ ({ target }) => {
+              if (target.value.length >= minNumber) {
+                this.setState({
+                  statusPassword: false,
+                  password: target.value }, () => this.updateBtnStatus());
+              } else {
+                this.setState({
+                  statusPassword: true,
+                  password: target.value }, () => this.updateBtnStatus());
+              }
+            } }
           />
         </label>
-        <button type="button">Entrar</button>
+        <button type="button" disabled={ btnStatus }>Entrar</button>
       </main>
     );
   }
