ec912d223aeb7afaabdfe4708a408545c5506ede
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-10-16 14:25:18 -0300

    2.feito validacao Email e senha, redux n configurado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      // email: '',
-      // password: '',
+      email: '',
+      password: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.checkPassword = this.checkPassword.bind(this);
+    this.checkEmail = this.checkEmail.bind(this);
+    this.clickButtonEntrar = this.clickButtonEntrar.bind(this);
   }
 
   handleChange({ target }) {
@@ -18,7 +22,35 @@ class Login extends React.Component {
     });
   }
 
+  // validação do email
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+  // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  checkEmail(email) {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(email);
+  }
+
+  // validação da senha
+  checkPassword(password) {
+    const seis = 6;
+    return (password.length >= seis);
+  }
+
+  // redirecionando para pagina carteira
+  clickButtonEntrar() {
+    const { history } = this.props;
+    history.push('/carteira');
+  }
+
   render() {
+    const {
+      email,
+      password,
+    } = this.state;
+
+    const isPasswordValid = this.checkPassword(password);
+    const isEmailValid = this.checkEmail(email);
+
     return (
       <form>
         <fieldset>
@@ -30,6 +62,7 @@ class Login extends React.Component {
               id="email"
               type="text"
               name="email"
+              value={ email }
               onChange={ this.handleChange }
               data-testid="email-input"
             />
@@ -40,16 +73,29 @@ class Login extends React.Component {
             <input
               type="password"
               name="password"
+              value={ password }
               data-testid="password-input"
               onChange={ this.handleChange }
             />
           </label>
 
-          <button type="button">Entrar</button>
+          <button
+            type="button"
+            disabled={ !(isPasswordValid && isEmailValid) }
+            onClick={ this.clickButtonEntrar }
+          >
+            Entrar
+          </button>
         </fieldset>
       </form>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
