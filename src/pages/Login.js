863a6e1e41062264ae389ffeff3e0af11606660c
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-16 11:23:56 -0300

    Creat emai and password field and button Entrar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            <input
+              type="text"
+              data-testid="email-input"
+              name="email"
+              defaultValue="email"
+            />
+          </label>
+          <label htmlFor="pasword">
+            <input
+              type="pasword"
+              data-testid="password-input"
+              name="pasword"
+              defaultValue="password"
+            />
+          </label>
+        </form>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
