55d091901e3522c1e21aa1985cf3282e29789dfd
Juliana Braga <juliana4braga@gmail.com>
2021-10-15 23:46:35 -0300

    Requisito 1 e começo do 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,47 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      availableButton: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          type="email"
+          placeholder="Digite seu email"
+          name="email"
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          placeholder="Insira sua senha"
+          minLength="6"
+          onChange={ this.handleChange }
+        />
+        <button type="button">Entrar</button>
+      </form>
+    );
   }
 }
 
