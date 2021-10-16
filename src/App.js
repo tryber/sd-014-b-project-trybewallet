1f3d7c31e90ece4bcbe674e05ace4aba52f8204d
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-16 11:11:45 -0300

    Creates Router to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="//carteira" component={ Wallet } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
