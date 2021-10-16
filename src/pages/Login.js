16c6ca3f9f14d3cae22b92deb7b25e3a4f5dabad
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-10-15 22:48:15 -0300

    [Rafael Moraes] Projeto TrybeWallet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <main>
+        <label htmlFor="email-input">
+          <input
+            type="text"
+            placeholder="Digite seu e-mail"
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            type="text"
+            placeholder="Digite sua senha"
+            data-testid="password-input"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </main>
+    );
   }
 }
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const walletReducer = (state = INITIAL_STATE) => state;
+
+export default walletReducer;
