c901996a273697d12323a5e3144a5b517dd9bac7
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-16 12:16:27 -0300

    Create Reducers

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,25 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { CURRENCIES, EXPENSES } from '../actions/index';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case CURRENCIES:
+    return {
+      ...state,
+      currencies: action.payload,
+    };
+  case EXPENSES:
+    return {
+      ...state,
+      expenses: action.payload,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
