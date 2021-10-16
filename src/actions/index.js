c817b457d3bb685ea3b90ae1c29e8adb8a6dc29c
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-10-16 00:33:48 -0300

    Requisitos 1 a 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
-// Coloque aqui suas actions
+export const SET_USER_EMAIL = 'SET_USER_EMAIL';
+export const SET_USER_PASSWORD = 'SET_USER_PASSWORD';
+
+export const setUserEmail = (payload) => ({
+  type: SET_USER_EMAIL,
+  payload,
+});
+export const setUserPassword = (payload) => ({
+  type: SET_USER_PASSWORD,
+  payload,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SET_USER_EMAIL, SET_USER_PASSWORD } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+    password: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SET_USER_EMAIL:
+    return { ...state, user: { email: action.payload, password: state.user.password } };
+  case SET_USER_PASSWORD:
+    return { ...state, user: { email: state.user.email, password: action.payload } };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
