f29a8fe4df8ed5f831433dcca8c6d5263bc48c55
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-10-17 20:32:45 -0300

    add requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,69 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disableButton: true,
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  isEmailValid(email) {
+    const regexEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return regexEmail.test(email) === true;
+  }
+  /* sugestão feita pelo Michael Caxias nesta thread: https://trybecourse.slack.com/archives/C023YHXAEGM/p1634319081263300 */
+
+  validateInputs() {
+    const { email, password } = this.state;
+    const passwordValid = 5;
+    if ((password.length >= passwordValid) && (this.isEmailValid(email))) {
+      this.setState({
+        disableButton: false,
+      });
+    }
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.id]: target.value,
+    });
+    this.validateInputs();
+  }
+
   render() {
+    const { email, password, disableButton } = this.state;
     return (
       <section>
         <input
           type="email"
-          name="email"
+          id="email"
+          value={ email }
+          onChange={ this.handleChange }
           placeholder="Digite seu Email"
           data-testid="email-input"
 
         />
         <input
           type="password"
-          name="senha"
+          id="password"
+          value={ password }
+          onChange={ this.handleChange }
           placeholder="Digite sua Senha"
           data-testid="password-input"
         />
 
-        <button type="button">Entrar</button>
+        <button
+          type="button"
+          disabled={ disableButton }
+        >
+          Entrar
+
+        </button>
 
       </section>
     );
