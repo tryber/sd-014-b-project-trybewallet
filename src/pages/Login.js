188731aed7d94cf87e1d89a59a03f01da4767bc5
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-10-17 01:56:48 -0300

    removido dir redux que causa erro no avaliador

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { setUserEmailValue, setUserPasswordValue } from '../redux/actions';
+import { setUserEmailValue, setUserPasswordValue } from '../actions';
 
 class Login extends React.Component {
   constructor() {
