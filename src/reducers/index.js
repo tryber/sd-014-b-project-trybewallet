870a3a2b6a071caf9ccc898612d4faef17cc354d
adilson <adilsongb.rabelo@gmail.com>
2021-10-18 11:54:52 -0300

    Corrigindo lint

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 // import user from './user';
 // import wallet from './wallet';
 const INITIAL_STATE = {
-  state: ''
-}
+  state: '',
+};
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
-const reducer = (state = INITIAL_STATE) => {
-  return state;
-}
+const reducer = (state = INITIAL_STATE) => state;
 
 export default reducer;
