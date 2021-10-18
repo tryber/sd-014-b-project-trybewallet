ee873e55effc91bddaee19e2ce8ea3f3147bb854
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-18 16:57:13 -0300

    configurando redux e requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import store from './store';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <BrowserRouter>
+          <Route exact path="/" component={ Login } />
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="e-mail"
+          placeholder="E-mail"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="senha"
+          placeholder="Senha"
+        />
+        <input
+          type="button"
+          name="entrar"
+          value="Entrar"
+        />
+      </form>
+    );
   }
 }
 
