4995e2933524c4fd9ea605ddb2174d196b590f38
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-10-18 18:39:20 -0300

    remove importaçao do bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './routes/Routes';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
