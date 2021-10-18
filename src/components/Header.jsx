589bc044f9386b95e697ee96b8085450a73a090d
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-10-18 15:53:40 -0300

    Requisito 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
@@ -18,5 +19,9 @@ class Header extends React.Component {
 }
 const mapStateToProps = (state) => ({
   email: state.user.email });
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps, null)(Header);
