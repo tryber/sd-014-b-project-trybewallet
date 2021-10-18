601434d371c3b777c91a32274dc136b7e3149d4e
lucas <mr.pine97@gmail.com>
2021-10-18 16:21:38 -0300

    configurando o reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const SAVE_EMAIL = 'SAVE_EMAIL';
 export const REQUEST_CURRENCIES = 'REQUEST_CURRENCIES';
 export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const FAILED_REQUEST = 'FAILED_REQUEST';
 
 const LIMIT_CONTROL = 3;
 
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,15 +1,22 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
+import { FAILED_REQUEST, GET_CURRENCIES, REQUEST_CURRENCIES } from '../actions';
+
 const initialState = {
   currencies: [],
   expenses: [],
+  isFetching: false,
+  error: '',
 };
 
 const wallet = (state = initialState, { type, payload }) => {
   switch (type) {
-  case 'typeName':
-    return { ...state, ...payload };
-
+  case REQUEST_CURRENCIES:
+    return { ...state, isFetching: true };
+  case GET_CURRENCIES:
+    return { ...state, currencies: payload, isFetching: false };
+  case FAILED_REQUEST:
+    return { ...state, error: payload, isFetching: false };
   default:
     return state;
   }
