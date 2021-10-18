af6b335f4e76ae017cb3a05325d6cfdf739a48f2
Leandro Xavier <leandrosxavi@gmail.com>
2021-10-18 18:30:14 -0300

    Adiciona requisitos 1 e 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,61 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleInputs = this.handleInputs.bind(this);
+  }
+
+  handleInputs({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    // Link de onde encontrei o Regex para a validação do email:
+    // https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
+    const valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const passMinLength = 6;
+    return (
+      <div>
+        <h2>Login</h2>
+        <form>
+          <input
+            data-testid="email-input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={ email }
+            onChange={ this.handleInputs }
+          />
+          <input
+            data-testid="password-input"
+            type="password"
+            name="password"
+            placeholder="Senha"
+            value={ password }
+            onChange={ this.handleInputs }
+          />
+          <input
+            type="button"
+            value="Entrar"
+            disabled={
+              !email.match(valid) || password.length < passMinLength
+            }
+            // onClick={}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
