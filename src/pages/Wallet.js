4a51b221ebafe71fed234f99a8cf32090a309fa2
Rafael Veiga <rafaelveig@hotmail.com>
2021-10-18 16:34:16 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props; // email do estado que é passado por props la no login page que é o estado compartilhado.
+
+    return (
+      <div>
+        TrybeWallet
+        <section>
+          <div>
+            Email:
+            <span data-testid="email-field">{email}</span>
+          </div>
+          <div>
+            Despesa total: R$
+            <span data-testid="total-field" />
+          </div>
+          <div data-testid="header-currency">BRL</div>
+        </section>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
