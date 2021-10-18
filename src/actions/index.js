b838af8bf42d6b7757de67e0847ef2b07d5a01ef
Fernando Serpa <serpaef@outlook.com>
2021-10-18 14:16:46 -0300

    impl: requisito 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,6 @@ const LOGIN = 'LOGIN';
 const ADD_EXPENSES = 'ADD_EXPENSES';
 const CURRENCIES = 'CURRENCIES';
 
-export const doLogin = (value) => ({ type: LOGIN, value });
+export const doLogin = (value = '') => ({ type: LOGIN, value });
 export const addExpenses = (value) => ({ type: ADD_EXPENSES, value });
 export const currencies = (value) => ({ type: CURRENCIES, value });
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,24 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">{ email }</span>
+        <span data-testid="header-currency-field">BRL</span>
+        <span data-testid="total-field">0</span>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({ email: state.user.email });
+
+export default connect(mapStateToProps, null)(Wallet);
