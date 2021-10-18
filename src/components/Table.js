32d9b847e9b729ad20f369f5502baa89c4df6339
LucasAccurcio <lucas.accurcio@gmail.com>
2021-10-18 20:14:50 -0300

    Requisito 09

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import './Table.css';
 
 class Table extends React.Component {
   render() {
+    const { purchases } = this.props;
+    let arrayPurchase = false;
+
+    if (purchases.length !== 0) {
+      arrayPurchase = true;
+    }
+
+    const renderPurchase = (
+      purchases.map((purchase) => {
+        const currency = purchase.exchangeRates[purchase.currency].name;
+        const currencyPurchase = currency.split('/');
+        const exchangeRate = purchase.exchangeRates[purchase.currency].ask;
+        const convertedValue = purchase.value * exchangeRate;
+        return (
+          <tr key={ purchase.id }>
+            <td>{ purchase.description }</td>
+            <td>{ purchase.tag }</td>
+            <td>{ purchase.method }</td>
+            <td>{ parseFloat(purchase.value) }</td>
+            <td>{ currencyPurchase[0] }</td>
+            <td>{ parseFloat(exchangeRate).toFixed(2) }</td>
+            <td>{ convertedValue.toFixed(2) }</td>
+            <td>Real</td>
+          </tr>
+        );
+      })
+    );
+
+    console.log(purchases);
     return (
       <table>
         <thead>
@@ -17,10 +48,21 @@ class Table extends React.Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          {arrayPurchase && renderPurchase}
         </thead>
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  purchases: PropTypes.arrayOf(PropTypes.shape({
+    map: PropTypes.func,
+  })).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  purchases: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
