420a9332d48a7d5bfb68f1c78f6cc0faabcf2d9c
andremorsch <andremorsch37@gmail.com>
2021-10-18 16:20:04 -0300

    bomba, pq esqueci de jogar na 1, tem a 2 e 3 juntas

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // Coloque aqui suas actions
+// const NEW_ACTION = 'NEW_ACTION';
+
+// export default const newAction = (payload) => ({ type: NEW_ACTION, payload });
+
+export const USER_ACTION = 'USER_ACTION';
+
+export const walletAction = (payload) => ({ type: USER_ACTION, payload });
