aa85580ba1325d64c3522b1cb3008cdd9c155f2c
lucas <mr.pine97@gmail.com>
2021-10-18 16:13:16 -0300

    criando as actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,37 @@
 export const SAVE_EMAIL = 'SAVE_EMAIL';
+export const REQUEST_CURRENCIES = 'REQUEST_CURRENCIES';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
+
+const LIMIT_CONTROL = 3;
 
 export const saveEmail = (email) => ({
   type: SAVE_EMAIL,
   email,
 });
+
+function requestCurrencies() {
+  return { type: REQUEST_CURRENCIES };
+}
+
+function getCurrencies(json) {
+  return {
+    type: GET_CURRENCIES,
+    payload: Object.keys(json).filter((currency) => currency.length === LIMIT_CONTROL),
+  };
+}
+
+function failedRequest(error) {
+  return { type: FAILED_REQUEST, payload: error };
+}
+
+export const fetchCurrencies = () => async (dispatch) => {
+  try {
+    dispatch(requestCurrencies());
+    const endpoint = 'https://economia.awesomeapi.com.br/json/all';
+    const fetchUrl = await fetch(endpoint);
+    const jsonParsing = await fetchUrl.json();
+    dispatch(getCurrencies(jsonParsing));
+  } catch (error) {
+    return dispatch(failedRequest(error));
+  }
+};
