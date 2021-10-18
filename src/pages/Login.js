ed38643f530169328c46d1e4ea2c5dfd937f2ada
Cecilia Cacais <ceci.cacais@gmail.com>
2021-10-18 16:33:55 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,21 +19,65 @@ guardar email e coloca-lo no header
 */
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+
+    this.state = {
+      email: '',
+      senha: '',
+      loginHabilitado: false,
+    };
+  }
+
+  handleTextAreaChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+    const { email, senha } = this.state;
+    const quatro = 4;
+    const re = /\S+@\S+\.\S+/; /* usei o site: https://www.horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/ */
+    if (re.test(email)) {
+      console.log(email);
+      if (senha.length > quatro) {
+        this.setState({
+          loginHabilitado: true,
+        });
+      }
+    }
+  }
+
   render() {
+    const { loginHabilitado, email, senha } = this.state;
     return (
       <div>
 
         <label htmlFor="email">
           Email:
-          <input id="email" data-testid="email-input" />
+          <input
+            name="email"
+            id="email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleTextAreaChange }
+          />
         </label>
 
         <label htmlFor="senha">
           Senha:
-          <input id="senha" data-testid="password-input" />
+          <input
+            name="senha"
+            id="senha"
+            data-testid="password-input"
+            value={ senha }
+            onChange={ this.handleTextAreaChange }
+          />
         </label>
 
         <button
+          disabled={ loginHabilitado === false }
           type="button"
         >
           Entrar
