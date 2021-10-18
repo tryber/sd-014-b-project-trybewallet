c4e483bb50852151933614875476d3b7ffcc46ba
daniel <daniel.acoelho@outlook.com>
2021-10-18 11:54:02 -0300

    Requisito 5

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+function Header({ user }) {
+  return (
+    <header>
+      <span data-testid="email-field">{user}</span>
+      <span data-testid="total-field">0</span>
+      <span data-testid="header-currency-field">BRL</span>
+    </header> 
+  );  
+}  
+
+Header.propTypes = {
+    user: PropTypes.string.isRequired,
+  };
+  
+  const mapStateToProps = (state) => ({ user: state.user.email });
+  
+  export default connect(mapStateToProps, null)(Header);
+  
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Wallet() {
-  return <div>TrybeWallet</div>;
+  return (
+    <section>
+      <Header />
+      TrybeWallet
+    </section>
+  );
 }
