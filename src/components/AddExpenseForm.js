74c51e8cf44e055fcf52813ba817868fc7bad359
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-10-18 14:46:41 -0300

    Requisito 6

---
+++ src/components/AddExpenseForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Input from './Input';
+import Select from './Select';
+
+class AddExpenseForm extends Component {
+  render() {
+    const { newExpense, onChange } = this.props;
+    const { amount, description, currency, paymentMethod, tag } = newExpense;
+    return (
+      <form>
+        <Input
+          label="Valor"
+          type="number"
+          id="amount"
+          value={ amount }
+          onChange={ onChange }
+        />
+        <Input
+          label="Descrição"
+          type="text"
+          id="description"
+          value={ description }
+          onChange={ onChange }
+        />
+        <Select label="Moeda" id="currency" value={ currency } onChange={ onChange }>
+          {}
+        </Select>
+        <Select
+          label="Método de pagamento"
+          id="paymentMethod"
+          value={ paymentMethod }
+          onChange={ onChange }
+        >
+          <option>Dinheiro</option>
+          <option>Cartão de crédito</option>
+          <option>Cartão de débito</option>
+        </Select>
+        <Select label="Tag" id="tag" value={ tag } onChange={ onChange }>
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </Select>
+      </form>
+    );
+  }
+}
+
+AddExpenseForm.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  newExpense: PropTypes.shape({
+    amount: PropTypes.number,
+    description: PropTypes.string,
+    currency: PropTypes.string,
+    paymentMethod: PropTypes.string,
+    tag: PropTypes.string,
+  }).isRequired,
+};
+
+export default AddExpenseForm;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import AddExpenseForm from '../components/AddExpenseForm';
 
 class Wallet extends React.Component {
   constructor() {
@@ -9,15 +10,35 @@ class Wallet extends React.Component {
 
     this.state = {
       totalExpense: 0,
+      newExpense: {
+        amount: 0,
+        description: '',
+        currency: '',
+        paymentMethod: '',
+        tag: '',
+      },
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState((state) => ({
+      newExpense: {
+        ...state.newExpense,
+        [name]: value,
+      },
+    }));
   }
 
   render() {
     const { email } = this.props;
-    const { totalExpense } = this.state;
+    const { totalExpense, newExpense } = this.state;
     return (
       <div>
         <Header userEmail={ email } totalExpense={ totalExpense } />
+        <AddExpenseForm newExpense={ newExpense } onChange={ this.handleChange } />
       </div>
     );
   }
