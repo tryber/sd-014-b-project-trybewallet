5c2771f3ce270e39ed5d9faec8bae513d6a568ad
davisralves <davialves324@gmail.com>
2021-10-18 20:01:52 -0300

    corrigindo app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
-import requestCurrency from './Api/api';
 
 class App extends React.Component {
   render() {
