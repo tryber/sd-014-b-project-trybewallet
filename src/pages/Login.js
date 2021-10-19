36d6d31344931cb59f5a07f0b80eba0486424721
perchespierri <perchespierri@gmail.com>
2021-10-19 13:28:27 -0300

    Requisito 1 -- Espaco para digitar email e senha criados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return(
+      <nav>
+        <label htmlFor="email-input">
+          E-mail
+          <input
+            data-testid="email-input"
+            name="emailInput"
+            type="email"
+            placeholder="Digite seu e-mail"
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha
+          <input
+            data-testid="password-input"
+            name="passwordInput"
+            type="password"
+            placeholder="Digite sua senha"
+          />
+        </label>
+      </nav>
+    );
   }
 }
 
