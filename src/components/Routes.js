74de398cd3ad9c8726ba28fa09206781c4f37c81
Jean Marcel <jeanvanzella91@gmail.com>
2021-10-19 03:21:46 -0300

    req 1 e 2

---
+++ src/components/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+import Wallet from '../pages/Wallet';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/carteira">
+          <Wallet />
+        </Route>
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,93 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      ableButton: true,
+      redirection: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleButton = this.handleButton.bind(this);
+    this.validateButton = this.validateButton.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+    this.validateButton();
+  }
+
+  validateButton() {
+    const { password } = this.state;
+    const five = 5;
+    if (this.validateEmail() && password.length > five) {
+      this.setState({
+        ableButton: false,
+      });
+    } else {
+      this.setState({
+        ableButton: true,
+      });
+    }
+  }
+
+  validateEmail() {
+    const { email } = this.state;
+    const splitedEmail = email.split('');
+    if (email.includes('@')
+    && splitedEmail[splitedEmail.length - 1] !== '@'
+    && splitedEmail[splitedEmail.length - 1] !== '.') {
+      return true;
+    }
+    return false;
+  }
+
+  handleButton() {
+    this.setState({
+      redirection: true,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { ableButton, email, password, redirection } = this.state;
+    return (
+      <form>
+        { redirection && <Redirect to="/carteira" />}
+        <label htmlFor="imput-email">
+          <input
+            id="imput-email"
+            type="email"
+            data-testid="email-input"
+            name="email"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="imput-password">
+          <input
+            id="imput-password"
+            type="password"
+            data-testid="password-input"
+            name="password"
+            onChange={ this.handleChange }
+            value={ password }
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ this.handleButton }
+          disabled={ ableButton }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
