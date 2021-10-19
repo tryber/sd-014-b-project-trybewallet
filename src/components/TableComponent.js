16068fd77d6d9adab21f6ce00bd7c31cad1fff00
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-10-19 18:05:20 -0300

    Requisito 9.1

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -7,40 +7,44 @@ class TableComponent extends React.Component {
     const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-        {expenses.map((
-          {
-            id,
-            description,
-            tag,
-            method,
-            value,
-            currency,
-            exchangeRates: {
-              [currency]: { ask, name } },
-          },
-        ) => (
-          <tr key={ id }>
-            <td>{description}</td>
-            <td>{tag}</td>
-            <td>{method}</td>
-            <td>{value}</td>
-            <td>{name}</td>
-            <td>{Number(ask).toFixed(2)}</td>
-            <td>{(Number(value) * Number(ask)).toFixed(2)}</td>
-            <td>Real</td>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {expenses.map((
+            {
+              id,
+              description,
+              tag,
+              method,
+              value,
+              currency,
+              exchangeRates: {
+                [currency]: { ask, name } },
+            },
+          ) => (
+            <tr key={ id }>
+              <td>{description}</td>
+              <td>{tag}</td>
+              <td>{method}</td>
+              <td>{value}</td>
+              <td>{name}</td>
+              <td>{Number(ask).toFixed(2)}</td>
+              <td>{(Number(value) * Number(ask)).toFixed(2)}</td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
