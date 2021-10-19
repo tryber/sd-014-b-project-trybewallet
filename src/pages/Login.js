ddea87a776d141208b611fe3b9f7f1626011f9ed
raphael-caninde <raphael.caninde@gmail.com>
2021-10-19 12:57:44 -0300

    requsito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,30 +2,72 @@ import React from 'react';
 import '../style/Login.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputEmail: '',
+      inputPassword: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.validateForms = this.validateForms.bind(this);
+  }
+
+  handleInputChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  validateForms() {
+    const { inputEmail, inputPassword } = this.state;
+    const minLength = 6;
+    const emailValidate = /\S+@\S+\.\S+/;
+    return emailValidate.test(inputEmail) && inputPassword.length >= minLength;
+  }
+
   render() {
+    const { inputEmail, inputPassword } = this.state;
+    const { validateForms } = this;
+
     return (
       <main id="container">
         <form>
-          <h2>Trybe Wallet</h2>
+          <h2>
+            Trybe
+            <p>Wallet</p>
+          </h2>
           <div>
             <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
               data-testid="email-input"
+              type="text"
+              name="inputEmail"
+              placeholder="Enter your email"
+              value={ inputEmail }
+              onChange={ this.handleInputChange }
             />
             <div className="underline" />
           </div>
           <div>
             <input
-              type="password"
-              name="password"
-              placeholder="Enter your password"
               data-testid="password-input"
+              type="password"
+              name="inputPassword"
+              placeholder="Enter your password"
+              value={ inputPassword }
+              onChange={ this.handleInputChange }
             />
             <div className="underline" />
           </div>
-          <input type="button" value="Entrar" />
+          <button
+            type="button"
+            value="Entrar"
+            className="buttonLogin"
+            disabled={ !validateForms() }
+          >
+            Entrar
+          </button>
         </form>
       </main>
     );
@@ -33,3 +75,8 @@ class Login extends React.Component {
 }
 
 export default Login;
+
+/* links que peguei de referencia para validação do email */
+/* https://pt.stackoverflow.com/questions/291974/valida%C3%A7%C3%A3o-campo-e-mails
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+https://pt.stackoverflow.com/questions/291974/valida%C3%A7%C3%A3o-campo-e-mails */
