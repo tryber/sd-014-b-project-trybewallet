06803f2e8dffce90ca6d2c44f79463fac36631a2
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-19 16:23:44 -0300

    cria handle change  e estado local de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <form>
         <input
@@ -9,17 +26,22 @@ class Login extends React.Component {
           data-testid="email-input"
           name="e-mail"
           placeholder="E-mail"
+          onChange={ (event) => this.handleChange(event) }
+          value={ email }
         />
         <input
           type="password"
           data-testid="password-input"
           name="senha"
           placeholder="Senha"
+          onChange={ (event) => this.handleChange(event) }
+          value={ password }
         />
         <input
           type="button"
           name="entrar"
           value="Entrar"
+          /* disable={  } */
         />
       </form>
     );
---
+++ src/reducers/user.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  email: '',
+};
+
+function userReducer(state, action) {
+  switch (action.type) {
+  case 'LOGIN':
+    return {};
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
