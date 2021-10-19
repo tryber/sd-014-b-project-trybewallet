0fce900a90844eecb3e507f20286f3abf42ad961
Patrick Paredes <patrickparedes44@gmail.com>
2021-10-19 19:30:13 -0400

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,64 @@
 import React from 'react';
+import { Redirect } from 'react-router';
+import validator from 'validator';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      redirect: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick() {
+    this.setState({
+      redirect: true,
+    });
+  }
+
   render() {
+    const { email, password, redirect } = this.state;
+    const min = 6;
+
+    if (redirect) return <Redirect to="/carteira" />;
+
     return (
       <section>
-        <input data-testid="email-input" type="email" placeholder="Email" />
-        <input data-testid="password-input" type="password" placeholder="Password" />
-        <button type="button">Entrar</button>
+        <input
+          data-testid="email-input"
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          disabled={ password.length < min || !validator.isEmail(email) }
+        >
+          Entrar
+        </button>
       </section>
     );
   }
