c06d7d875964a6281f7ea323bacb7f55bebf59ce
Bruno M Landim <brunolandim13@hotmail.com>
2021-10-19 15:46:52 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './store/index';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={ store }>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <input
+          name="email"
+          value={ email }
+          data-testid="email-input"
+          type="email"
+          placeholder="Digite seu email"
+          onChange={ this.handleChange }
+          required
+        />
+        <input
+          name="password"
+          value={ password }
+          data-testid="password-input"
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={ this.handleChange }
+          required
+        />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
