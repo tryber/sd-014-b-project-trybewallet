a64827c51b1c2d8f133a18f0d9bd8bcd0365bb2e
Cecilia Cacais <ceci.cacais@gmail.com>
2021-10-19 17:09:25 -0300

    Requisito 4

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Login';
+import Wallet from './Wallet';
 
 class Content extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
