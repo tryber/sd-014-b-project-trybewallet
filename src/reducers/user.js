a5fb49604eb0e4d9a8c02653831f4fb90e1700c4
perchespierri <perchespierri@gmail.com>
2021-10-19 14:16:58 -0300

    Requisito 3 - Criado o user reducer que pega o email

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SAVE_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) { // Lembrando que a action se chama saveEmailAction e recebe email
+  case (SAVE_EMAIL):
+    return ({
+      ...state,
+      email: action.payload,
+    });
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
