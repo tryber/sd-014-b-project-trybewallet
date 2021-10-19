aa834b5fe551bfd40805911f8cbad0cd2c118077
Rafael Veiga <rafaelveig@hotmail.com>
2021-10-18 21:16:17 -0300

    req 6 problema no combobox?

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,24 +3,72 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
-  render() {
-    const { email } = this.props; // email do estado que é passado por props la no login page que é o estado compartilhado.
+  constructor(props) {
+    super(props);
+    this.header = this.header.bind(this);
+  }
 
+  header() {
+    const { email } = this.props; // email do estado que é passado por props la no login page que é o estado compartilhado.
+    return (
+      <section className="header_container">
+        <div>
+          Email:
+          <span data-testid="email-field">{email}</span>
+        </div>
+        <div>
+          Despesa total: R$
+          <span data-testid="total-field">0</span>
+        </div>
+        <div data-testid="header-currency-field">BRL</div>
+      </section>
+    );
+  }
+
+  render() {
     return (
       <div>
         TrybeWallet
-        <section>
-          <div>
-            Email:
-            <span data-testid="email-field">{email}</span>
-          </div>
-          <div>
-            Despesa total: R$
-            <span data-testid="total-field">0</span>
-          </div>
-          <div data-testid="header-currency">BRL</div>
-          <div data-testid="header-currency-field">BRL</div>
-        </section>
+        {this.header()}
+        <form className="add_despesas_container">
+          <label htmlFor="valor">
+            Valor:
+            <input
+              type="number"
+              min="0.0"
+              step="0.01"
+              name="valor"
+              id="valor"
+            />
+          </label>
+          <label htmlFor="Descricao">
+            Descrição
+            <input type="text" name="Descricao" id="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda:
+            <select name="moeda" id="moeda">
+              <option value="BRL">BRL</option>
+            </select>
+          </label>
+          <label htmlFor="pagamento" id="pagamento">
+            <select name="metodo_de_pagamento" id="pagamento">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag:
+            <select name="tag" id="tag">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer" selected>Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
