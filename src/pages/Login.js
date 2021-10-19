e5bf7964f9431556ecc6b592c2e7024845cae91f
Paulo França <paulovinicius.franca@gmail.com>
2021-10-19 16:20:01 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form className="login">
+          <h1>TrybeWallet</h1>
+          <input data-testid="email-input" type="text" placeholder="Digite o seu e-mail" />
+          <input data-testid="password-input" type="password" placeholder="Digite a sua senha" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
