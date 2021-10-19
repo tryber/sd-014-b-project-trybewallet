407e3c780bf0fd3de87dada10f3d8bc88bb94062
Anderson Leite <andersonleite.dev@gmail.com>
2021-10-19 11:01:56 -0300

    setting user.js of reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,28 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SET_USER_DATA } from '../actions';
+
+/**
+ * Consultei o repositorio exercise-forms-redux para
+ * estudar e compreender como implementar:
+ * - user
+ * Ref: https://github.com/andersonleite1/exercise-forms-redux/blob/gabarito.2/src/redux/reducer/reducer.js
+ */
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SET_USER_DATA:
+    return {
+      ...state,
+      email: action.payload.email,
+      password: action.payload.password,
+    };
+  default:
+    return state;
+  }
+};
+
+export default user;
