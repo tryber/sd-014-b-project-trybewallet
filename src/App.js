53555a27389b97cb537ac5fad4864248662242d6
icaroemanuel <icaro.emanuel@outlook.com>
2021-10-19 10:00:25 -0300

    Req 1 - Página inicial de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/" render={ () => <Login /> } />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <label htmlFor="email-input">
+          Login:
+          <br />
+          <input
+            type="email"
+            name="email"
+            id="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="email-input"
+          />
+        </label>
+        <br />
+        <label htmlFor="password-input">
+          Senha:
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="password-input"
+          />
+        </label>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
-
-export default Login;
