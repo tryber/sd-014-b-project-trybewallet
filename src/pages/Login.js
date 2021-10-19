4b12da8bd88206848e1e8818b70a9efa1d24ed46
Thales Lima <thales.bplima@gmail.com>
2021-10-18 23:37:36 -0300

    impl req2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,30 +2,59 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.formValidation = this.formValidation.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.id]: target.value,
+    });
+  }
+
+  formValidation() {
+    const { email, password } = this.state;
+    const emailValidation = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const emailTest = emailValidation.test(email);
+    const MIN_LENGTH = 6;
+    const passwordTest = password.length >= MIN_LENGTH;
+    return emailTest && passwordTest;
+  }
+
   render() {
+    const { email, password } = this.state;
     const { history } = this.props;
     return (
       <form>
         <label htmlFor="email">
           E-Mail:
           <input
-            type="text"
+            value={ email }
+            type="email"
             data-testid="email-input"
             id="email"
-            /* onChange={  } */
+            onChange={ this.handleChange }
           />
         </label>
         <label htmlFor="password">
-          E-Mail:
+          Senha:
           <input
-            type="text"
+            value={ password }
+            type="password"
             data-testid="password-input"
             id="password"
-            /* onChange={  } */
+            onChange={ this.handleChange }
           />
         </label>
         <button
           type="button"
+          disabled={ !this.formValidation() }
           onClick={ () => (history.push('/carteira')) } // fazer handleSubmit
         >
           Entrar
