fa3a53811a7541692d4ea358698f3d78d69dcb5e
Anderson Leite <andersonleite.dev@gmail.com>
2021-10-19 11:06:53 -0300

    setting index.js (setUserData) of actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+/**
+ * Consultei o repositorio da atividade exercise-forms-redux
+ * para compreender a logica necessaria para criar uma action
+ * Ref: https://github.com/andersonleite1/exercise-forms-redux/blob/gabarito.2/src/redux/action/action.js
+ */
+
+export const SET_USER_DATA = 'SET_USER_DATA';
+
+export const setUserData = (payload) => (
+  {
+    type: SET_USER_DATA, payload,
+  });
