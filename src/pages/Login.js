b49f5ce32aa468dcf8e3e217e266a9b96e54a57b
dsomarcelo <celoygm@gmail.com>
2021-10-18 21:02:35 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,13 @@
 // import user from './user';
 // import wallet from './wallet';
 
+const INITIAL_STATE = {
+  state: '',
+};
+
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+const reducer = (state = INITIAL_STATE) => state;
+
+export default reducer;
