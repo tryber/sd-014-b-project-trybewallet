267f02589dfda2274ceba769342817f8519761b2
adilson <adilsongb.rabelo@gmail.com>
2021-10-19 13:49:05 -0300

    Correcao lint R10

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,22 +5,53 @@ import { updateExpenses } from '../actions';
 
 class Table extends React.Component {
   deleteItem(id) {
-    const { expenses, updateExpenses } = this.props;
+    const { expenses, submitUpdateExpenses } = this.props;
     const mutedExpenses = [...expenses];
     mutedExpenses.forEach((expense, index) => {
       if (expense.id === id) {
         mutedExpenses.splice(index, 1);
       }
     });
-    /* 
+    /*
       Poderia atualizar o id de todas as despesas toda vez que
       o array atualizar:
 
       mutedExpenses.forEach((expense, i) => {
         expense.id = i;
-      }); 
+      });
     */
-    updateExpenses(mutedExpenses);
+    submitUpdateExpenses(mutedExpenses);
+  }
+
+  renderItemsTable(expense, index) {
+    const nameCurrency = expense.exchangeRates[expense.currency].name;
+    let exchange = parseFloat(expense.exchangeRates[expense.currency].ask);
+    let convertedValue = expense.value * exchange;
+    exchange = exchange.toFixed(2);
+    convertedValue = convertedValue.toFixed(2);
+
+    return (
+      <tr key={ index }>
+        <td>{ expense.description }</td>
+        <td>{ expense.tag }</td>
+        <td>{ expense.method }</td>
+        <td>{ expense.value }</td>
+        <td>{ nameCurrency.split('/')[0] }</td>
+        <td>{ exchange }</td>
+        <td>{ convertedValue }</td>
+        <td>Real</td>
+        <td>
+          <button type="button">Editar</button>
+          <button
+            type="button"
+            data-testid="delete-btn"
+            onClick={ () => this.deleteItem(expense.id) }
+          >
+            Excluir
+          </button>
+        </td>
+      </tr>
+    );
   }
 
   render() {
@@ -41,35 +72,7 @@ class Table extends React.Component {
           </tr>
         </thead>
         <tbody>
-          { expenses.map((expense, i) => {
-            const nameCurrency = expense.exchangeRates[expense.currency].name;
-            let exchange = parseFloat(expense.exchangeRates[expense.currency].ask);
-            let convertedValue = expense.value * exchange;
-            exchange = exchange.toFixed(2);
-            convertedValue = convertedValue.toFixed(2);
-            return (
-              <tr key={ i }>
-                <td>{ expense.description }</td>
-                <td>{ expense.tag }</td>
-                <td>{ expense.method }</td>
-                <td>{ expense.value }</td>
-                <td>{ nameCurrency.split('/')[0] }</td>
-                <td>{ exchange }</td>
-                <td>{ convertedValue }</td>
-                <td>Real</td>
-                <td>
-                  <button type="button">Editar</button>
-                  <button
-                    type="button"
-                    data-testid="delete-btn"
-                    onClick={ () => this.deleteItem(expense.id) }
-                  >
-                    Excluir
-                  </button>
-                </td>
-              </tr>
-            );
-          }) }
+          { expenses.map((expense, i) => this.renderItemsTable(expense, i)) }
         </tbody>
       </table>
     );
@@ -79,14 +82,14 @@ class Table extends React.Component {
 Table.propTypes = {
   expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
   updateExpenses: PropTypes.func.isRequired,
-}
+};
 
 const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  updateExpenses: (expenses) => dispatch(updateExpenses(expenses)),
+  submitUpdateExpenses: (expenses) => dispatch(updateExpenses(expenses)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Table);
