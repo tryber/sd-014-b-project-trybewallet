f9a232e00719cd63a1a197eeffe715474784869c
GustavoDamm <gustavo.damm15@hotmail.com>
2021-10-20 15:40:36 -0300

    req 1

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,10 +1,8 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
 const INITIAL_STATE = {
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
+  currencies: [],
+  expenses: [],
 };
 
 const wallet = (state = INITIAL_STATE) => state;
