5e7af8f5b55514c818c7597ac04a94ad86e9e929
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-10-19 23:58:07 -0300

    Adicionando esqueleto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          data-testid="password-input"
+        />
+        <button type="button">
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
-
 export default Login;
