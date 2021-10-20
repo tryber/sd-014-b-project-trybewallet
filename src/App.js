4f89a32b66a0803b1e855d916042ec37106f96ce
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-20 11:36:12 -0400

    Testa o fetch na api de moedas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <div>Hello, TrybeWallet!</div>
+      <Wallet />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+
+    this.fetchAPI = this.fetchAPI.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI() {
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  }
+
   render() {
     return <div>TrybeWallet</div>;
   }
