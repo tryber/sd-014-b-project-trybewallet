5ffe93757478ad36137c71e52784097a8cbd54ad
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-20 11:47:47 -0400

    Configuração inicial da rota para login e carteira

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <>
-      <div>Hello, TrybeWallet!</div>
-      <Wallet />
+      <header>
+        <h1>Hello, TrybeWallet!</h1>
+      </header>
+      <Switch>
+        <Route path="/carteira" component={ Wallet } />
+        <Route exat path="/" component={ Login } />
+      </Switch>
     </>
   );
 }
