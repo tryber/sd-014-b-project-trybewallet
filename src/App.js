ca34d843e08f68a3cf8b336566195bb2b24c740c
dhenycruz <dheniarley.ds@gmail.com>
2021-10-19 21:57:17 -0300

    Requisito 1.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import store from './store';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Provider store={ store }>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Provider>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <form>
+          <input name="email" data-testid="email-input" />
+          <input name="password" data-testid="password-input" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
