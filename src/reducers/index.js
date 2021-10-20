50a68723d48b150e3dae1339a391d32fff5c1162
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-20 11:35:22 -0300

    salvando email

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import user from './user';
+import wallet from './wallet';
 
-const rootReducer = combineReducers({ });
+const rootReducer = combineReducers({ user, wallet });
 
 export default rootReducer;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,13 @@
+import { LOGIN } from '../actions';
+
 const INITIAL_STATE = {
   email: '',
 };
 
 function userReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-  case 'LOGIN':
-    return {};
+  case LOGIN:
+    return ({ ...state, email: action.email });
   default:
     return state;
   }
