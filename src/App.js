ce6183aef354ffa0fabf297df59fa800637d23a8
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-20 12:42:12 -0400

    Configuração inicial da página de login

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <header>
         <h1>Hello, TrybeWallet!</h1>
       </header>
@@ -13,7 +14,7 @@ function App() {
         <Route path="/carteira" component={ Wallet } />
         <Route exat path="/" component={ Login } />
       </Switch>
-    </>
+    </div>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,58 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    console.log(name + value);
+    this.setState({
+      user: {
+        [name]: value,
+      },
+    });
+  }
+
   render() {
+    const { user: { email, password } } = this.state;
     return (
       <div>
-        Login
+        <label htmlFor="email-input">
+          <input
+            id="email-input"
+            data-testid="email-input"
+            type="text"
+            placeholder="seu email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            id="password-input"
+            data-testid="password-input"
+            type="text"
+            placeholder="senha"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
