1e7473cba376d5f3cf01dbaefd700bbdfff6ca95
igo <igorhdsouza@gmail.com>
2021-10-20 09:30:15 -0300

    iniciando projeto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        Login
+        <form>
+          <label htmlFor="emailInput">
+            Email:
+            <input
+              type="email"
+              name="emailInput"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="passwordInput">
+            Senha:
+            <input
+              type="password"
+              name="passwordInput"
+              data-testid="password-input"
+            />
+          </label>
+          <button
+            type="button"
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SEND_LOGIN:
+    return {
+      email: action.email,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
