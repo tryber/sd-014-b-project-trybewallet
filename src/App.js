0a3f9548e34faafb0c3ef641a5afb4efcc591c25
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-20 12:16:15 -0300

    muda o browser e o provider para o index

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
-import store from './store';
+import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={ store }>
-        <BrowserRouter>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/carteira" component={ Wallet } />
-        </BrowserRouter>
-      </Provider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
     );
   }
 }
