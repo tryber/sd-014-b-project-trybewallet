deb451e98e3a6fa0cc61f5d6f22676ff29762025
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-19 22:14:14 -0400

    Configura o index do reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import reducer from '../reducers';
+import rootReducer from '../reducers';
 
 const store = createStore(
-  reducer, composeWithDevTools(applyMiddleware(thunk)),
+  rootReducer, composeWithDevTools(applyMiddleware(thunk)),
 );
 
 export default store;
