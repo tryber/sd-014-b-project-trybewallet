7dbb02e25800f1d574ee752dc30350214bf25316
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-10-22 17:40:31 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,54 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.validateEmail = this.validateEmail.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateEmail() {
+    const { email, password } = this.state;
+    const regex = /\S+@\S+\.\S+/.test(email);
+    const seis = 6;
+    const correctPassword = password.length >= seis;
+    return !regex || !correctPassword;
+  } // referência: https://trybecourse.slack.com/archives/C023YHXAEGM/p1634586091402700
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  } // referência: https://github.com/tryber/sd-014-b-project-trybewallet/pull/86/
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <section>
         <input
+          name="email"
           type="email"
           data-testid="email-input"
+          onChange={ this.handleChange }
+          value={ email }
         />
         <input
+          name="password"
           type="password"
           data-testid="password-input"
+          onChange={ this.handleChange }
+          value={ password }
         />
-        <button type="button">
+        <button
+          type="button"
+          disabled={ this.validateEmail() }
+        >
           Entrar
         </button>
       </section>
