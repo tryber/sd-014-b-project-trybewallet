982676f03b90ead1bef2ad2b66b0ed3a6c21af4d
uetiari <arianeueti@gmail.com>
2021-10-21 22:28:02 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Routes from './pages/Routes';
+import store from './store/store';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <Provider store={ store }>
+        <Routes />
+      </Provider>
     </BrowserRouter>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,69 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.inputValid = this.inputValid.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  inputValid() {
+    const { email, password } = this.state;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email); // Usei essa Theread para verificar email https://trybecourse.slack.com/archives/C023YHXAEGM/p1634586091402700
+
+    const SEIS = 6;
+    const correctPassword = password.length >= SEIS;
+
+    return !regex || !correctPassword;
+    // usei esse Repo de referência para esta função
+    // https://github.com/tryber/sd-014-b-project-trybewallet/pull/86/
+  }
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <div>
         <label htmlFor="email">
           Email:
-          <input type="text" id="email" data-testid="email-input" />
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            value={ email }
+            onChange={ this.handleChange }
+          />
         </label>
         <label htmlFor="password">
           Senha:
-          <input type="text" id="password" data-testid="password-input" />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            value={ password }
+            onChange={ this.handleChange }
+          />
         </label>
 
-        <button type="button"> Entrar </button>
+        <button
+          type="button"
+          disabled={ this.inputValid() }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
