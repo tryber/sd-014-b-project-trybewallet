a229d6349e7f4ad9d07881e1aa6e1306751d5f70
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-10-23 20:30:16 -0300

    fetchAPI resolved problem

--- src/services/economyAPI.js
+++ src/services/economyAPI.js
@@ -1,5 +1,5 @@
-async fetchAPI = () => {
-  const request = await fetch("https://economia.awesomeapi.com.br/json/all");
+fetchAPI = async () => {
+  const request = await fetch('https://economia.awesomeapi.com.br/json/all');
   const fetchData = request.json();
   console.log(fetchData);
-}
+};
