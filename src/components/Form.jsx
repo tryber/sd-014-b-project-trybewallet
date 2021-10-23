ac8fddadf7ba67944a8fe08a9e2a17b4838dc342
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-10-23 20:41:01 -0300

    baby steps in fomr and table

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
-class Form extends Component {
-  static propTypes = {
-    prop: PropTypes,
-  }
+// class Form extends Component {
+//   static propTypes = {
+//     prop: PropTypes,
+//   }
 
-  render() {
-    return (
-      <form>
-        <label htmlFor="name">
-          Nome:
-          <input type="text" name="name" />
-        </label>
-      </form>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <form>
+//         <label htmlFor="name">
+//           Nome:
+//           <input type="text" name="name" />
+//         </label>
+//       </form>
+//     );
+//   }
+// }
 
-export default Form;
+// export default Form;
--- src/components/TableExpenses.jsx
+++ src/components/TableExpenses.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+// import React, { Component } from 'react';
+// // import PropTypes from 'prop-types';
 
-class TableExpenses extends Component {
-  render() {
-    return (
-      <table>
-        <tbody>
-          <tr>
-            "//""
-          </tr>
-          <tr>
-            //
-          </tr>
-          <tr>
-            //
-          </tr>
-          <tr>
-            //
-          </tr>
-          <tr>
-            //
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-}
+// class TableExpenses extends Component {
+//   render() {
+//     return (
+//       <table>
+//         <tbody>
+//           <tr>
+//             "//""
+//           </tr>
+//           <tr>
+//             //
+//           </tr>
+//           <tr>
+//             //
+//           </tr>
+//           <tr>
+//             //
+//           </tr>
+//           <tr>
+//             //
+//           </tr>
+//         </tbody>
+//       </table>
+//     );
+//   }
+// }
 
-// TableExpenses.propTypes = {
-//   prop: PropTypes
-// };
+// // TableExpenses.propTypes = {
+// //   prop: PropTypes
+// // };
 
-export default TableExpenses;
+// export default TableExpenses;
