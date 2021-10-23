a2d3fc33790485ab50e81191bcd0bfd174d4d13f
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-10-23 14:20:12 -0300

    Implementa os elementos do requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,49 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      // email: '',
+      // password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <input
+            id="email-input"
+            data-testid="email-input"
+            type="email"
+            placeholder="Digite seu email"
+            name="email"
+            onChange={ this.handleChange }
+          />
+          <input
+            id="passord-input"
+            data-testid="password-input"
+            type="password"
+            name="password"
+            placeholder="Insira sua senha"
+            onChange={ this.handleChange }
+          />
+        </form>
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
