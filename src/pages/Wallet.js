f4c85c84f3ce62bb3e7f1676194949688314e00f
Cecilia Cacais <ceci.cacais@gmail.com>
2021-10-23 17:15:45 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,6 +6,41 @@ class Wallet extends React.Component {
     return (
       <main>
         <Header />
+        <form>
+          <label htmlFor="valor">
+            Valor
+            <input id="valor" type="text" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input id="descricao" type="text" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              <option>api</option>
+            </select>
+          </label>
+          <label htmlFor="metodoDePagamento">
+            Método de pagamento
+            <select id="metodoDePagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="categoria">
+              Tag
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </main>
     );
   }
