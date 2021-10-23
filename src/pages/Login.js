6de522eb0a3b80732322ab3b7e00f61994362292
Jéssica Queiroz <gnr0034@gmail.com>
2021-10-23 15:00:29 -0300

    logica redux + requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <label htmlFor="input-email">
+          Email:
+          <input
+            id="input-email"
+            name="email"
+            type="email"
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="input-password">
+          Password:
+          <input
+            id="input-password"
+            name="password"
+            type="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button
+          type="button"
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
