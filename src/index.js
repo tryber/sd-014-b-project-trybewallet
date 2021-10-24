4353cc50e023e9232377435cd748b50e3a754a75
Renato Bispo <renatoslvbispo@gmail.com>
2021-10-24 00:03:33 -0300

    Finish setting up MUI with fonts and icons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
