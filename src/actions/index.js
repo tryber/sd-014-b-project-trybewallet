9ce2f5cdbbabc822ee64e136f665413b2bdfc97f
Nasser <nasseralmeida@gmail.com>
2021-10-24 17:23:02 -0300

    impl: tela de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+
+export const user = (payload) => ({
+  type: LOGIN,
+  payload,
+});
+
+export const addExpense = (payload) => ({
+  type: ADD_EXPENSE,
+  payload,
+});
