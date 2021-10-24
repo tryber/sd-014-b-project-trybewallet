7075f9013ae1961af31ea43854ee995cebf32c23
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-10-24 17:56:59 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+import store from './store/store';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Provider store={ store }>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    </Provider>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: 'alguem@email.com',
+      password: '',
+      isDisabled: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { email, password } = target;
+    const validHandleChange = this.handleEmail(email) && this.handlePass(password);
+    this.setState({ isDisabled: !validHandleChange });
+  }
+
+  handleEmail(email) {
+    const regexEmail = /\S+@\S+\.\S+/;
+    return regexEmail.test(email);
+  }
+
+  handlePass(password) {
+    const caracterPass = 6;
+    return password.length >= caracterPass;
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isDisabled } = this.state;
+    const { submit } = this.props;
+    return (
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          id="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="text"
+          data-testid="password-input"
+          name="password"
+          id="password-input"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          onClick={ () => submit(email && password) }
+          disabled={ isDisabled }
+        >
+          <Link to="/carteira">
+            Entrar
+          </Link>
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispathParam) => ({
+  submit: (state) => dispathParam(submit(state)),
+});
+
+Login.propTypes = {
+  submit: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
