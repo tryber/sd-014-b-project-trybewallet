c6f0279979e5793064f2cee4b4f4292b32962d87
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-10-24 13:39:53 -0300

    requisito 07

--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import fetchQuotes from '../services';
 
 class ExpensesForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      quotes: [],
+    };
+
+    this.requestQuotes = this.requestQuotes.bind(this);
+  }
+
+  componentDidMount() {
+    this.requestQuotes();
+  }
+
+  async requestQuotes() {
+    const quotes = await fetchQuotes();
+    delete quotes.USDT;
+    this.setState({
+      quotes,
+    });
+  }
+
   render() {
+    const { quotes } = this.state;
+
     return (
       <form>
         <label htmlFor="value">
@@ -21,7 +46,9 @@ class ExpensesForm extends Component {
         <label htmlFor="coin">
           Moeda
           <select id="coin">
-            <option>BRL</option>
+            {
+              Object.keys(quotes).map((quote) => <option key={ quote }>{quote}</option>)
+            }
           </select>
         </label>
         <label htmlFor="payment-method">
