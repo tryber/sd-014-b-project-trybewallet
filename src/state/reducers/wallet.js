ab0800cdb8555aa60813a51cca4a28c1940641e0
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-24 10:05:47 -0400

    Constroi o reducer wallet

--- src/state/reducers/wallet.js
+++ src/state/reducers/wallet.js
@@ -1,9 +1,30 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { REQUEST_API_START, REQUEST_API_SUCCESS } from '../actions/currencies';
+
 const INITIAL_STATE = {
   currencies: [],
   expenses: [],
+  isLoading: true,
 };
 
-const wallet = (state = INITIAL_STATE) => state;
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case REQUEST_API_START: {
+    return {
+      ...state,
+      isLoading: action.payload.isLoading,
+    };
+  }
+  case REQUEST_API_SUCCESS: {
+    return {
+      ...state,
+      currencies: action.payload.currencies,
+      isLoading: action.payload.isLoading,
+    };
+  }
+  default:
+    return state;
+  }
+};
 
 export default wallet;
