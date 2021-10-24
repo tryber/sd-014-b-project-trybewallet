f73b7e56f65422896386f0de0e7e0579ec250772
Jéssica Queiroz <gnr0034@gmail.com>
2021-10-24 11:57:38 -0300

    Organizando src/

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { saveEmailInState } from '../actions';
+import { saveEmailInState } from '../redux/actions';
 
 class Login extends React.Component {
   constructor() {
