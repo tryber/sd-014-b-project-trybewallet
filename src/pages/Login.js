69b500f9cff48c3326b62afa9c800824259a1efd
Renato Bispo <renatoslvbispo@gmail.com>
2021-10-23 23:49:20 -0300

    Revert "Create layout and input components"
    This reverts commit e3b1fc42cd7b4f599fced071f9763ea993905c5e.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import Input from '../components/Input';
-import PageContainer from '../components/PageContainer';
 
 class Login extends React.Component {
   render() {
-    return (
-      <PageContainer>
-        <h1>Login</h1>
-        <Input type="email" id="email" name="email" placeholder="Email" />
-        <Input type="password" id="password" name="password" placeholder="Senha" />
-      </PageContainer>
-    );
+    return <div>Login</div>;
   }
 }
 
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import PageContainer from '../components/PageContainer';
 
 class Wallet extends React.Component {
   render() {
-    return (
-      <PageContainer>
-        <h1>Carteira</h1>
-      </PageContainer>
-    );
+    return <div>TrybeWallet</div>;
   }
 }
 
