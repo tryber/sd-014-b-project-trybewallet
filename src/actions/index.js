5147063d37654f860ed4e8d21a37428d55fd5910
uetiari <arianeueti@gmail.com>
2021-10-24 14:56:25 -0300

    Configurando Actions corretamente

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,19 @@
 // Coloque aqui suas actions
-const VALID_EMAIL = (email) => ({
-  type: VALID_EMAIL,
-  email,
+export const VALID_USER = 'VALID_USER';
+export const VALID_WALLET = 'VALID_WALLET';
+export const VALID_WALLET_EXPENSES = 'VALID_WALLET_EXPENSES';
+
+export const validUser = (payload) => ({
+  type: VALID_USER,
+  payload,
 });
 
-export default VALID_EMAIL;
+export const validWallet = (payload) => ({
+  type: VALID_WALLET,
+  payload,
+});
+
+export const validWalletExpenses = (payload) => ({
+  type: VALID_WALLET_EXPENSES,
+  payload,
+});
