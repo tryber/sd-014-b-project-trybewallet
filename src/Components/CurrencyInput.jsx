6b672222c7fd609c0e900ba9fa48c8dbda080ec4
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-24 13:31:38 -0300

    requisito 7

--- src/Components/CurrencyInput.jsx
+++ src/Components/CurrencyInput.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import getCurrencies from '../services/currencyApi';
 
 class CurrencyInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      apiCurrencies: [],
       currencies: [],
     };
+    this.getCurrenciesFromServices = this.getCurrenciesFromServices.bind(this);
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCurrenciesFromServices();
+  }
+  // Consultei https://github.com/tryber/sd-014-b-project-trybewallet/pull/21/files e tive ajuda do proprio.
+
+  async getCurrenciesFromServices() {
+    const dataApi = await getCurrencies();
+    this.setState({
+      apiCurrencies: [dataApi],
+    });
+    this.getData();
+  }
+
+  getData() {
+    const { apiCurrencies } = this.state;
+    apiCurrencies.map((currencyKeys) => (
+      this.setState({
+        currencies: Object.keys(currencyKeys),
+      })
+    ));
   }
 
   render() {
@@ -14,9 +40,14 @@ class CurrencyInput extends Component {
       <label htmlFor="currency">
         Moeda
         <select id="currency" name="currency-input">
-          {currencies.map((currency) => (
-            <option key={ currency.code }>{ currency.code }</option>
-          ))}
+          {currencies.map((currencie) => {
+            if (currencie !== 'USDT') {
+              return (
+                <option key={ currencie }>{ currencie }</option>
+              );
+            }
+            return '';
+          })}
         </select>
       </label>
     );
--- src/services/currencyApi.js
+++ src/services/currencyApi.js
@@ -1,7 +1,6 @@
 const getCurrencies = async () => {
   const fetchCurrencies = await fetch('https://economia.awesomeapi.com.br/json/all');
   const dataCurrencies = await fetchCurrencies.json();
-  console.log(dataCurrencies);
   return dataCurrencies;
 };
 
