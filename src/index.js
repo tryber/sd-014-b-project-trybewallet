e2d838428a27786696b7552bb775f76cdb6859b1
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-10-25 13:07:31 -0300

    evaluator fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import store from './redux/store';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
