e11dabd015a37a17fa3cea339c6fdca0dde87552
Nasser <nasseralmeida@gmail.com>
2021-10-25 10:51:45 -0300

    impl: pagina da carteira - estrutura inicial do form

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,7 +4,40 @@ class Expenses extends Component {
   render() {
     return (
       <div>
-        expenses form
+        <form>
+          <label htmlFor="input-value">
+            Valor:
+            <input type="text" id="input-value" />
+          </label>
+          <label htmlFor="input-description">
+            Descrição:
+            <input type="text" id="input-description" />
+          </label>
+          <label htmlFor="input-currency">
+            Moeda:
+            <select id="input-currency">
+              <option> </option>
+            </select>
+          </label>
+          <label htmlFor="input-method">
+            Método de pagamento
+            <select id="input-method">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="cartao-credito">Cartão de crédito</option>
+              <option value="cartao-debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="input-tag">
+            Tag
+            <select id="input-tag">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
