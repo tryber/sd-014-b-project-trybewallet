9ff6d07708e444cbc5ad752b58ba14993dd8ff10
Jéssica Queiroz <gnr0034@gmail.com>
2021-10-25 20:31:36 -0300

    Tentativa 2 - anexando pastas (actions, reducers, store) diretamente no scr

--- src/components/FormWallet.jsx
+++ src/components/FormWallet.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { saveExpensesInState } from '../redux/actions';
+import { saveExpensesInState } from '../actions';
 import Inputs from './Inputs';
 import SelectCoin from './SelectCoin';
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { saveEmailInState } from '../redux/actions';
+import { saveEmailInState } from '../actions';
 
 class Login extends React.Component {
   constructor() {
