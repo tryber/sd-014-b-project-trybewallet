4cb7f20b94deca0d792b7a0e3aa71837ef67b830
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-10-25 18:49:47 -0300

    Requisito 6 - Correção de nome da label

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,32 +4,28 @@ class Expenses extends Component {
   render() {
     return (
       <form>
-
         <label htmlFor="value">
           Valor
           <input type="text" name="value" id="value" />
         </label>
-
         <label htmlFor="description">
           Descrição
           <input type="text" name="description" id="description" />
         </label>
-
         <label htmlFor="currency">
           Moeda
-          <select name="currency" id="currency">Moeda</select>
+          <select name="currency" id="currency">{}</select>
         </label>
-
         <label htmlFor="payment-method">
           Método de pagamento
           <select name="payment-method" id="payment-method">
             <option value="dinheiro">Dinehiro</option>
-            <option value="cartao-de-credito">Cartão de Crédito</option>
-            <option value="cartao-de-debito">Cartão de Débito</option>
+            <option value="cartao-de-credito">Cartão de crédito</option>
+            <option value="cartao-de-debito">Cartão de débito</option>
           </select>
         </label>
-
         <label htmlFor="tag">
+          Tag
           <select name="tag" id="tag">
             <option value="alimentacao">Alimentação</option>
             <option value="lazer">Lazer</option>
