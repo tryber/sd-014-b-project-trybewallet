9b9c6b8f1cdc3b459f5dd4802b35e385e96ac499
unknown <el_dantas1999@hotmail.com>
2021-10-25 14:36:23 -0300

    Requisito 09

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/header';
 import Form from '../components/form';
+import ExpensesTable from '../components/expensesTable';
 
 class Wallet extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <main>
         <Header />
         <Form />
+        <ExpensesTable />
       </main>
     );
   }
