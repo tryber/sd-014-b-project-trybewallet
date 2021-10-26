e8ee25e4d969b1be73a395b01ba8dfe622e17eea
lucas <mr.pine97@gmail.com>
2021-10-26 14:08:15 -0300

    impl: req09 - renderiza a tabela

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import WalletTable from '../components/WalletTable';
 
 class Wallet extends React.Component {
   render() {
+    const { expensesFromGlobalState } = this.props;
     return (
       <>
         <Header />
         <WalletForm />
+        { expensesFromGlobalState.length === 0 ? '' : <WalletTable /> }
       </>
     );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => (
+  {
+    expensesFromGlobalState: state.wallet.expenses,
+  }
+);
+
+Wallet.propTypes = {
+  expensesFromGlobalState: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
