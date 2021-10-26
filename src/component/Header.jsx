1e1b0f403a418bc485572eba143abf059741ec66
Cezar <cezarguimaraes@gmail.com>
2021-10-26 00:56:52 -0300

    Requisito 7 first try

---
+++ src/component/Header.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <span data-testid="email-field">
+          {`Email: ${email}`}
+        </span>
+        <span data-testid="total-field">
+          0
+        </span>
+        <span data-testid="header-currency-field">
+          BRL
+        </span>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,77 @@
 import React from 'react';
+import Header from '../component/Header';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      coins: [],
+    };
+
+    this.fetchCoins = this.fetchCoins.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCoins();
+  }
+
+  async fetchCoins() {
+    const fetchedCoins = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const coins = await fetchedCoins.json();
+    const coinsArray = Object.keys(coins);
+    this.setState({
+      coins: coinsArray.filter((item) => item !== 'USDT'),
+    });
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { coins } = this.state;
+    const coinsArray = coins.map((coin, index) => (
+      <option key={ index }>
+        {coin}
+      </option>
+    ));
+    console.log(coins);
+    return (
+      <div>
+        <Header />
+        <form>
+          <label htmlFor="valor">
+            Valor:
+            <input type="text" name="valor" id="valor" />
+          </label>
+          <label htmlFor="descrição">
+            Descrição:
+            <input type="text" name="descrição" id="descrição" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda:
+            <select name="moeda" id="moeda">
+              { coinsArray }
+            </select>
+          </label>
+          <label htmlFor="Método de pagamento">
+            Método de pagamento:
+            <select name="Método de pagamento" id="Método de pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag" name="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
