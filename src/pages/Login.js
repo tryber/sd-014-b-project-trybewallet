ffa9c2f702b1016c142529e1d7946f7c5bca0c0c
cassiomasc <csmlvx@gmail.com>
2021-10-26 01:50:21 -0300

    Requisito 01

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return <div>
+      <input data-testid="email-input" type="email"/>
+      <input data-testid="password-input" type="password"/>
+      <button type="button">Entrar</button>
+    </div>;
   }
 }
 
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { REQUEST_USER, SAVE_USER } from '../actions';
+
+const INITIAL_STATE = {
+  user: {
+    email: '',
+    senha: '',
+  },
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case REQUEST_USER:
+    return state;
+  case SAVE_USER:
+    return action.value;
+  default:
+    return state;
+  }
+}
+
+export default user;
