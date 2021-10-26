1611d4873c8269b540442b4a5853eab977e33b45
Renato Bispo <renatoslvbispo@gmail.com>
2021-10-26 00:20:18 -0300

    Setup redux store and reducers (no actions)

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,11 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.payload) {
+  default:
+    return state;
+  }
+}
