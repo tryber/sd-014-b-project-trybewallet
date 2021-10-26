17e770d7fcd82df8913e92ce31f2be13c7535465
Renato Bispo <renatoslvbispo@gmail.com>
2021-10-26 00:21:29 -0300

    Finish requirement 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,47 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+
+    return (
+      <main>
+        <h2>Login</h2>
+        <div>
+          <form>
+            <input
+              type="email"
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+              data-testid="email-input"
+            />
+            <input
+              type="password"
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
+              data-testid="password-input"
+            />
+            <button type="submit">Entrar</button>
+          </form>
+        </div>
+      </main>
+    );
   }
 }
 
