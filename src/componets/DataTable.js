0cf2bec49ef5a3806b7219fb7851fa40e47da154
cassiomasc <csmlvx@gmail.com>
2021-10-26 22:36:00 -0300

    req 9 com começo do 10

--- src/componets/DataTable.js
+++ src/componets/DataTable.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class DataTable extends React.Component {
   render() {
+    const { purchases } = this.props;
+    let setItensArray = false;
+    setItensArray = purchases.length !== 0;
+
+    const renderPurchase = (
+      purchases.map((purchase) => {
+        const currency = purchase.exchangeRates[purchase.currency].name;
+        const currencyPurchase = currency.split('/');
+        const exchangeRate = purchase.exchangeRates[purchase.currency].ask;
+        const convertedValue = purchase.value * exchangeRate;
+        return (
+          <tr key={ purchase.id }>
+            <td>{ purchase.description }</td>
+            <td>{ purchase.tag }</td>
+            <td>{ purchase.method }</td>
+            <td>{ parseFloat(purchase.value) }</td>
+            <td>{ currencyPurchase[0] }</td>
+            <td>{ parseFloat(exchangeRate).toFixed(2) }</td>
+            <td>{ convertedValue.toFixed(2) }</td>
+            <td>Real</td>
+            <td>
+              <button type="button" data-testid="edit-btn">edita</button>
+              <button type="button" data-testid="delete-btn" onClick="">apaga</button>
+            </td>
+          </tr>
+        );
+      })
+    );
     return (
       <table>
         <thead>
@@ -16,10 +46,21 @@ class DataTable extends React.Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+          {setItensArray && renderPurchase}
         </thead>
       </table>
     );
   }
 }
 
-export default DataTable;
+DataTable.propTypes = {
+  purchases: PropTypes.arrayOf(
+    PropTypes.shape.isRequired,
+  ).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  purchases: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(DataTable);
