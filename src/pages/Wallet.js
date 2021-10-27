d6c999fca7d6ae6e38d283cdfbe6bf1a41d07dca
igo <igorhdsouza@gmail.com>
2021-10-27 09:42:42 -0300

    requisito 06

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import ExpenseForm from '../components/ExpenseForm';
 
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
     return (
-      <header>
-        <span data-testid="email-field">{email}</span>
-        <span data-testid="total-field">0</span>
-        <span data-testid="header-currency-field">BRL</span>
-      </header>
+      <div>
+        <header>
+          <span data-testid="email-field">{email}</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        <ExpenseForm />
+      </div>
     );
   }
 }
