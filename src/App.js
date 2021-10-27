f80fd14be14236ad2cad90707f419bed2de81db8
igo <igorhdsouza@gmail.com>
2021-10-27 08:49:21 -0300

    requisito 05

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/carteira" />
+        <Route path="/carteira" component={ Wallet } />
         <Route exact path="/" render={ (props) => <Login { ...props } /> } />
       </Switch>
     );
