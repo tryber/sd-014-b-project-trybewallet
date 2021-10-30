e73442a3311bb87c796e6f3c121b58391ab74984
Nataniel Santos <nathan.santos159@hotmail.com>
2021-10-30 14:08:42 -0300

    Corrigindo eslint

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -6,7 +6,7 @@ import TableRow from './TableRow';
 class ExpensesTable extends Component {
   render() {
     const { expenses } = this.props;
-    console.log(expenses)
+    console.log(expenses);
     return (
       <table>
         <thead>
