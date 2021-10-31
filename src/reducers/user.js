b1c43e04bd01954b008d134bf01089298783ec0b
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-10-31 13:00:53 -0300

    redux initial config

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case '':
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
