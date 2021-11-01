90aa4ac589de2cd6810e7e40fa5e627840b808e0
uetiari <arianeueti@gmail.com>
2021-11-01 15:52:07 -0300

    Ajustes de Lint

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,24 +1,4 @@
-// import React, { Components } from 'react';
-
-// class Input extends Components {
-//   render() {
-//     const { title, id, valor, onChange } = this.props;
-//     return (
-//       <label htmlFor={ id }>
-//         { title }
-//         <input
-//           type="text"
-//           id={ id }
-//           value={ valor }
-//           onChange={ onChange }
-//         />
-//       </label>
-//     );
-//   }
-// }
-
-// export default Input;
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Input extends Component {
