10490fb6b2190fedc6817de2af4aab49691a2e2e
natalia <mellomartins.naty@gmail.com>
2021-11-01 02:01:15 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,69 @@
 import React from 'react';
 
+/* O email está no formato válido, como 'alguem@alguem.com'.
+
+A senha possui 6 ou mais caracteres.
+
+Salve o email no estado da aplicação, com a chave email, assim que a pessoa usuária logar.
+
+A rota deve ser mudada para '/carteira' após o clique no botão 'Entrar'. */
+
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.allowSubmit = this.allowSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  allowSubmit() {
+    const { email, password } = this.state;
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+
+    const SEIS = 6;
+    const correctPassword = password.length >= SEIS;
+
+    return !regex || !correctPassword;
+  } // Referência: https://github.com/tryber/sd-014-b-project-trybewallet/pull/3/files
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
-        <input type="email" data-testid="email-input" placeholder="e-mail" />
-        <input type="password" data-testid="password-input" placeholder="senha" />
-        <button type="button">Entrar</button>
+        <h1>Login</h1>
+        <input
+          type="email"
+          data-testid="email-input"
+          placeholder="e-mail"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="senha"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ this.allowSubmit() }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
