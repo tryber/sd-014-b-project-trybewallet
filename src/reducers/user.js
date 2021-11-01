f319019a38da4dd0b8adaa4c970d2983456719c0
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-11-01 08:41:10 -0300

    login finalizado

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,16 @@
+import { SAVE_USER_EMAIL } from '../actions/userActions';
+
 const INITIAL_STATE = {
   email: '',
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case '':
-    return state;
+  case SAVE_USER_EMAIL:
+    return {
+      ...state,
+      email: action.email,
+    };
   default:
     return state;
   }
