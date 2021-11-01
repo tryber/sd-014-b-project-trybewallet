dce2f41725fc64f2735767b888247f058e3995f6
natalia <mellomartins.naty@gmail.com>
2021-11-01 05:01:49 -0300

    requisito 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,8 @@ class Header extends React.Component {
     return (
       <header>
         <p data-testid="email-field">{ email }</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
       </header>
     );
   }
