684a22cfe0bfecac8363395a291a48918ce07ec9
icaroemanuel <icaro.emanuel@outlook.com>
2021-11-02 15:30:50 -0300

    Requisito 9

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -74,6 +74,49 @@ class Wallet extends React.Component {
     );
   }
 
+  renderTable() {
+    const { expenseTable } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenseTable.length === 0
+              ? (<tr><td>Adicione uma despesa</td></tr>) : expenseTable
+                .map(({ id, value, description, currency, method, tag, exchangeRates,
+                }) => (
+                  <tr key={ id }>
+                    <td>{description}</td>
+                    <td>{tag}</td>
+                    <td>{ method }</td>
+                    <td>{ value}</td>
+                    <td>{exchangeRates[currency].name.split('/')[0]}</td>
+                    <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                    <td>
+                      {(Number(value) * Number(exchangeRates[currency].ask)).toFixed(2)}
+                    </td>
+                    <td>Real</td>
+                    <td>Editar/Excluir</td>
+                  </tr>
+                ))
+          }
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     const { userEmail } = this.props;
     const { value, description } = this.state;
@@ -100,6 +143,7 @@ class Wallet extends React.Component {
           {this.renderSelects()}
           <Button onClick={ this.handleClick } textButton="Adicionar despesa" />
         </form>
+        {this.renderTable()}
       </>
     );
   }
@@ -108,6 +152,7 @@ class Wallet extends React.Component {
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
   currencies: state.wallet.currencies,
+  expenseTable: state.wallet.expenses,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -120,6 +165,7 @@ Wallet.propTypes = {
   currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
   fetchCoinsApi: PropTypes.func.isRequired,
   expense: PropTypes.func.isRequired,
+  expenseTable: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
