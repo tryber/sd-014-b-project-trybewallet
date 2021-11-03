1dea924ad41dc3fd19c8f30593e036e0e65f91d4
Tiago Ponce <tiagocponce@gmail.com>
2021-11-03 15:56:49 -0300

    Requirement 01

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // Coloque aqui suas actions
+
+export const SUBMIT_USER = 'SUBMIT_USER';
+
+export const saveEmail = (email) => ({
+  type: SUBMIT_USER,
+  payload: email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+// import { saveEmail } from '../actions';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="email" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Login);
