b4c13c4fb73cc3f44529abb975eaf268a87514a0
= <luz.matheus11@gmail.com>
2021-11-04 15:15:32 -0300

    Requisito 4, 5 e 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      spent: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    const { spent } = this.state;
+    return (
+      <header>
+        <h3 data-testid="email-field">{ userEmail }</h3>
+        <form>
+          <label htmlFor="money-by" data-testid="total-field">
+            Valor:
+            <input
+              type="number"
+              id="money-by"
+            />
+            { spent }
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select data-testid="header-currency-field" id="moeda">
+              <option>BRL</option>
+            </select>
+          </label>
+          <label htmlFor="pagamento">
+            Método de pagamento
+            <select id="pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="descrição">
+            Descrição:
+            <input type="text" id="descrição" />
+          </label>
+        </form>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+export default connect(mapStateToProps)(Wallet);
