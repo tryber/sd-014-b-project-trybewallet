caee1d5ccd16747514685af8ae6e9132f0349331
Tiago Ponce <tiagocponce@gmail.com>
2021-11-04 10:46:23 -0300

    Requiremenet 07

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -6,6 +6,40 @@ class Wallet extends React.Component {
     return (
       <div>
         <Header />
+        <form>
+          <label htmlFor="Valor">
+            Valor
+            <input type="text" id="Valor" />
+          </label>
+          <label htmlFor="Descricao">
+            Descrição
+            <input type="text" id="Descricao" />
+          </label>
+          <label htmlFor="Coins">
+            Moeda
+            <select id="Coins">
+              <option>BRL</option>
+            </select>
+          </label>
+          <label htmlFor="payment">
+            Método de pagamento
+            <select id="payment">
+              <option type="combobox">Dinheiro</option>
+              <option type="combobox">Cartão de crédito</option>
+              <option type="combobox">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tags">
+            Tag
+            <select id="tags">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
