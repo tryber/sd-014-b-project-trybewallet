4989ae08cf25c46c568b5c79ce1306af05c8677f
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-11-07 19:40:19 -0300

    salvando a organizaçao

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './Store';
+import store from './Store/index';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
