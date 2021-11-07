0c337faf03ddab077d100a8b6d2f42dd9e91c64b
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-11-07 20:17:30 -0300

    sss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
-import Wallet from './Pages/Wallet';
 
 function App() {
   return (
     <main>
       <Switch>
-        <Route path="/carteira" component={ Wallet } />
         <Route path="/" component={ Login } />
       </Switch>
     </main>
