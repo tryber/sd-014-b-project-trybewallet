44e3cefb22bb13d7e7c4e5842257b92694db5c0c
Thiago Gomez <thiagogomez2906@hotmail.com>
2021-11-07 17:19:17 -0300

    requisito 1 ao 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,85 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userAction } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      senha: '',
+      verify: true,
+    };
+    this.uptadeState = this.uptadeState.bind(this);
+    this.submit = this.submit.bind(this);
+    this.verify = this.verify.bind(this);
+  }
+
+  uptadeState(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    this.verify();
+  }
+
+  checkEmail(email) {
+    const check = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+    return check.test(email);
+  }
+
+  verify() {
+    const { email, senha } = this.state;
+    const min = 5;
+    if (this.checkEmail(email) && senha.length >= min) {
+      this.setState({ verify: false });
+    } else { this.setState({ verify: true }); }
+  }
+
+  submit() {
+    const { email } = this.state;
+    const { history, setEmail } = this.props;
+    setEmail(email);
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, senha, verify } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            data-testid="email-input"
+            onChange={ this.uptadeState }
+            value={ email }
+            name="email"
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            onChange={ this.uptadeState }
+            value={ senha }
+            name="senha"
+          />
+          <button
+            type="button"
+            disabled={ verify }
+            onClick={ this.submit }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  setEmail: (email) => dispatch(userAction(email)),
+});
 
-export default Login;
+export default connect(null, mapDispatchToProps)(Login);
+
+Login.propTypes = {
+  history: PropTypes.shape().isRequired,
+  setEmail: PropTypes.func.isRequired,
+};
