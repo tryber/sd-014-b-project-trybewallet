3ec9207b9d7fb78de124c9c482bb56ad93bd1c9f
IgorFellows <igor.fpp96@gmail.com>
2021-11-08 14:09:13 -0300

    será que vai?

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
