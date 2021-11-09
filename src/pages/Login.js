e7fc40b9837cb01bf2f46a1c8bc8f7a3e6fc6181
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-11-09 13:39:38 -0300

    nao desisto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import { loginInfoAction } from '../actions';
 
 class Login extends React.Component {
