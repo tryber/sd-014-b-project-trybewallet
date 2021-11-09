04cb8a2aba5443e578bc2e603999f18514685894
Eicke Yumoto <eickeyumoto@gmail.com>
2021-11-09 12:23:17 -0300

    requsiitos 5 e 6

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import ExpensesLabel from './ExpensesLabel';
+import SelectLabel from './SelectLabel';
+
+const paymentMethods = [
+  {
+    value: 'cash',
+    content: 'Dinheiro',
+  },
+  {
+    value: 'creditCard',
+    content: 'Cartão de crédito',
+  },
+  {
+    value: 'debitCard',
+    content: 'Cartão de débito',
+  },
+];
+const tag = [
+  {
+    value: 'food',
+    content: 'Alimentação',
+  },
+  {
+    value: 'recreation',
+    content: 'Lazer',
+  },
+  {
+    value: 'work',
+    content: 'Trabalho',
+  },
+  {
+    value: 'transport',
+    content: 'Transporte',
+  },
+  {
+    value: 'health',
+    content: 'Saúde',
+  },
+];
+
+class Expenses extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      description: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { value, description } = this.state;
+    return (
+      <form>
+        <ExpensesLabel
+          text="Valor: "
+          type="number"
+          onChange={ this.handleChange }
+          value={ value }
+          name="value"
+        />
+        <ExpensesLabel
+          text="Descrição: "
+          type="text"
+          onChange={ this.handleChange }
+          value={ description }
+          name="description"
+        />
+        <SelectLabel
+          html="currency"
+          text="Moeda"
+          option={ [{ content: 'URL', value: '5,17' }] }
+        />
+        <SelectLabel
+          html="paymentMethod"
+          text="Método de pagamento"
+          option={ paymentMethods }
+        />
+        <SelectLabel
+          html="expenses"
+          text="Tag"
+          option={ tag }
+        />
+      </form>
+    );
+  }
+}
+
+export default Expenses;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from '../components/Header';
+import Expenses from '../components/Expenses';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+        <div>
+          <Expenses />
+        </div>
+      </div>
+    );
   }
 }
 
