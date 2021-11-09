7acd09f31151249e628aa0e763503e674ef574f8
Eicke Yumoto <eickeyumoto@gmail.com>
2021-11-09 12:56:07 -0300

    requisito 7

---
+++ src/helper/data.jsx
@@ -0,0 +1,31 @@
+export const inpHelper = [
+  {
+    type: 'text',
+    id: 'descricao',
+    text: 'Descrição',
+    name: 'description',
+  },
+  {
+    type: 'number',
+    text: 'Valor',
+    id: 'value',
+    name: 'value',
+  },
+];
+
+export const selectHelper = [
+  {
+    text: 'Método de pagamento',
+    name: 'method',
+    id: 'method',
+    options: ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'],
+  },
+  {
+    text: 'Tag',
+    name: 'tag',
+    id: 'tag',
+    options: ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'],
+  },
+];
+
+// ajuda do https://github.com/tryber/sd-012-project-trybewallet/pull/5/commits/5a21e3a55956f58087c49dee02a64a208c063ce0#
