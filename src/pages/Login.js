939df575c950fe9a95b01abf172617645dc94282
Diego Costa <dgrcosta@gmail.com>
2021-11-09 08:48:03 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <input type="email" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="button">Entrar</button>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Login);
