175db62556870e94daf8084d05e181210c88d246
Victor Martins <victor.martins.duarte@gmail.com>
2021-11-09 04:30:32 -0300

    Requirements 1 and 2 Login page and Validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+const emailValidation = (email) => {
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return emailRegex.test(email) === true;
+}; // Source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript;
+
+function Login() {
+  const [email, changeEmail] = useState('');
+  const [password, changePassword] = useState('');
+
+  const limit = 6;
+  const btnDisable = password.length >= limit && emailValidation(email);
+  return (
+    <form action="">
+      <input
+        data-testid="email-input"
+        type="email"
+        placeholder="E-mail"
+        value={ email }
+        onChange={ ({ target: { value } }) => changeEmail(value) }
+      />
+      <input
+        data-testid="password-input"
+        type="password"
+        placeholder="Senha"
+        value={ password }
+        onChange={ ({ target: { value } }) => changePassword(value) }
+      />
+      <button
+        type="button"
+        disabled={ !btnDisable }
+      >
+        Entrar
+      </button>
+    </form>
+  );
 }
 
 export default Login;
