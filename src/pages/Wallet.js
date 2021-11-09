fc00e4313c6eebfa82a6a59c6f080ccfc1219483
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-11-09 13:42:15 -0300

    vamo vamo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
+import Header from '../components/header';
 import InputComponent from '../components/InputComponent';
 import SelectComponent from '../components/SelectComponent';
 import { currenciesAction, expensesAction, editedExpensesAction } from '../actions';
