f6a4f0f27803c735439d46765e8857cd136ebc80
Nataniel Santos <nathan.santos159@hotmail.com>
2021-12-15 17:21:36 -0300

    Updates

---
+++ precache-manifest.bab005399cbec926ba4789b31d2ff762.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665a1ef9c4f106563b886b125f397cfc",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "daa5d75bbe16f8d11a1a",
+    "url": "/trybe-wallet/static/css/main.055a6895.chunk.css"
+  },
+  {
+    "revision": "22caa54547b39bced008",
+    "url": "/trybe-wallet/static/js/2.4cd1ceaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.4cd1ceaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa5d75bbe16f8d11a1a",
+    "url": "/trybe-wallet/static/js/main.93883334.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  },
+  {
+    "revision": "f0ee19e49594e61e39b12df8546b47e7",
+    "url": "/trybe-wallet/static/media/background.f0ee19e4.svg"
+  }
+]);
